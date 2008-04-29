31b2698a37a68921c20b2c0045a533afd626e330
PJ Hyett <pjhyett@gmail.com>
2008-04-29 16:20:10 -0700

    fix twitter bug and remove some old code

--- services/twitter.rb
+++ services/twitter.rb
@@ -1,18 +1,16 @@
 service :twitter do |data, payload|
-  repository = payload['repository']['name']
-  branch     = payload['ref'].split('/').last
   twitter    = Twitter::Base.new(data['username'], data['password'])
-  begin
-    Timeout::timeout(2) do
-      url = Net::HTTP.get "tinyurl.com", "/api-create.php?url=#{commit['url']}"
-    end
-  rescue
-  end
-  url ||= commit['url']
+  repository = payload['repository']['name']
 
   payload['commits'].each do |commit|
     commit = commit.last
-    text   = "[#{repository}] #{url} #{commit['author']['name']} - #{commit['message']}"
-    twitter.post text
+    begin
+      Timeout::timeout(2) do
+        url = Net::HTTP.get "tinyurl.com", "/api-create.php?url=#{commit['url']}"
+      end
+    rescue
+    end
+    url ||= commit['url']
+    twitter.post "[#{repository}] #{url} #{commit['author']['name']} - #{commit['message']}"
   end
 end
