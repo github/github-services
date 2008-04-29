a26abf301d9300820ebfe0826aba63e3748d4b88
Tekkub <tekkub@gmail.com>
2008-04-29 16:18:44 -0600

    Add cia.vc service

---
+++ services/cia.rb
@@ -0,0 +1,33 @@
+
+service :cia do |data, payload|
+  server = XMLRPC::Client.new2("http://cia.navi.cx")
+
+  repository = payload['repository']['name']
+  branch     = payload['ref'].split('/').last
+  payload['commits'].each do |sha1, commit|
+    message = %Q|
+<message>
+  <generator>
+    <name>github</name>
+    <version>1</version>
+    <url>http://www.github.com</url>
+  </generator>
+  <source>
+    <project>#{repository}</project>
+    <branch>#{branch}</branch>
+  </source>
+  <timestamp>#{commit['timestamp'].to_i}</timestamp>
+  <body>
+    <commit>
+      <author>#{commit['author']['name']}</author>
+      <revision>#{sha1[0..6]}</revision>
+      <log>#{commit['message']}</log>
+      <url>#{commit['url']}</url>
+    </commit>
+  </body>
+</message>
+|
+
+    result = server.call("hub.deliver", message)
+  end
+end
