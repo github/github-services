aef4f141ada1673a9a5ceffa2ebdb19a439bea2f
John Reilly <jr@trms.com>
2008-04-28 22:22:46 -0500

    Added FogBugz to list of services.
    params[:data] must include "cvs_submit_url" which points to the php/asp page in the Fogbugz install that will receive the commit notifications.

--- github-services.rb
+++ github-services.rb
@@ -1,5 +1,5 @@
 $:.unshift *Dir["#{File.dirname(__FILE__)}/vendor/**/lib"]
-%w( rack sinatra tinder twitter json net/http socket timeout ).each { |f| require f }
+%w( rack sinatra tinder twitter json net/http net/https socket timeout ).each { |f| require f }
 
 post '/campfire/' do
   data       = JSON.parse(params[:data])
@@ -14,6 +14,49 @@ post '/campfire/' do
     commit = commit.last
     text   = "[#{repository}/#{branch}] #{commit['message']} - #{commit['author']['name']} (#{commit['url']})"
     room.speak text
+  end
+end
+
+post '/fogbugz/' do
+  data        = JSON.parse(params[:data])
+  payload     = JSON.parse(params[:payload])
+
+  repository  = payload['repository']['name']
+  branch      = payload['ref'].split('/').last
+  before      = payload['before']   
+  
+  payload['commits'].each do |commit_id, commit|
+    message = commit["message"]
+    files   = commit["removed"] | commit["added"] | commit["modified"]
+    
+    # look for a bug id in each line of the commit message
+    bug_list = []
+    message.split("\n").each do |line|
+      if (line =~ /\s*Bug[zs]*\s*IDs*\s*[#:; ]+((\d+[ ,:;#]*)+)/i)
+        bug_list << $1.to_i
+      end
+    end
+    
+    # for each found bugzid, submit the files to fogbugz.
+    bug_list.each do |fb_bugzid|
+      files.each do |f|
+        fb_repo = CGI.escape("#{repository}")
+        fb_r1 = CGI.escape("#{before}")
+        fb_r2 = CGI.escape("#{commit_id}")
+        fb_file = CGI.escape("#{branch}/#{f}")
+        
+        #build the GET request, and send it to fogbugz
+        fb_url = "#{data['cvs_submit_url']}?ixBug=#{fb_bugzid}&sRepo=#{fb_repo}&sFile=#{fb_file}&sPrev=#{fb_r1}&sNew=#{fb_r2}"
+        url = URI.parse(fb_url)
+        conn = Net::HTTP.new(url.host, url.port)
+        conn.use_ssl = true
+        conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        conn.start do |http| 
+          puts http.get(url.to_s)
+        end
+
+      end
+    end
   end
 end
 
