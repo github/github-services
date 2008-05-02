d2abfee0383beaccb5a0a9b1511b965d07210ac4
PJ Hyett <pjhyett@gmail.com>
2008-05-02 15:47:24 -0700

    slightly sexier irc name, couple more thanks

--- services/irc.rb
+++ services/irc.rb
@@ -2,7 +2,7 @@ service :irc do |data, payload|
   repository = payload['repository']['name']
   branch     = payload['ref'].split('/').last
   rooms      = data['room'].gsub(",", " ").split(" ").map{|room| room[0].chr == '#' ? room : "##{room}"}
-  botname    = "github_updater#{rand(200)}"
+  botname    = "GitHub#{rand(200)}"
   irc        = TCPSocket.open(data['server'], data['port'])
   irc.puts "USER #{botname} #{botname} #{botname} :GitHub IRCBot"
   irc.puts "NICK #{botname}"
