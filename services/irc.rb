9c910274d07f2812b74214e6be713765f9b94d3d
Sean O'Brien <sobrien@bleh.local>
2008-05-06 15:00:41 -0700

    added ssl support to irc

--- services/irc.rb
+++ services/irc.rb
@@ -3,7 +3,17 @@ service :irc do |data, payload|
   branch     = payload['ref'].split('/').last
   rooms      = data['room'].gsub(",", " ").split(" ").map{|room| room[0].chr == '#' ? room : "##{room}"}
   botname    = "GitHub#{rand(200)}"
-  irc        = TCPSocket.open(data['server'], data['port'])
+  socket     = TCPSocket.open(data['server'], data['port'])
+  if data['ssl'].to_i == 1
+    ssl_context = OpenSSL::SSL::SSLContext.new()
+    ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE             # do not verify client certificates
+    ssl_socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
+    ssl_socket.sync_close = true
+    ssl_socket.connect
+    irc = ssl_socket
+  else
+    irc = socket
+  end
   irc.puts "PASS #{data['password']}" unless data['password'].empty?
   irc.puts "USER #{botname} #{botname} #{botname} :GitHub IRCBot"
   irc.puts "NICK #{botname}"
