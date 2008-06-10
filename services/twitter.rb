456c3b20829e64a4ae92f1fd480fda57761cdb5c
PJ Hyett <pjhyett@gmail.com>
2008-06-09 23:34:07 -0700

    twitter gem was overboard for making a simple post request

--- services/twitter.rb
+++ services/twitter.rb
@@ -1,16 +1,22 @@
 service :twitter do |data, payload|
-  twitter    = Twitter::Base.new(data['username'], data['password'])
   repository = payload['repository']['name']
+  url = URI.parse("http://twitter.com/statuses/update.xml")
 
   payload['commits'].each do |commit|
     commit = commit.last
     begin
       Timeout::timeout(2) do
-        url = Net::HTTP.get "tinyurl.com", "/api-create.php?url=#{commit['url']}"
+        github_url = Net::HTTP.get "tinyurl.com", "/api-create.php?url=#{commit['url']}"
       end
     rescue
     end
-    url ||= commit['url']
-    twitter.post "[#{repository}] #{url} #{commit['author']['name']} - #{commit['message']}"
+    github_url ||= commit['url']
+    status = "[#{repository}] #{github_url} #{commit['author']['name']} - #{commit['message']}"
+
+    req = Net::HTTP::Post.new(url.path)
+    req.basic_auth(data['username'], data['password'])
+    req.set_form_data('status' => status, 'source' => 'github')
+
+    Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
   end
 end
