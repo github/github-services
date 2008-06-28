9b55771f97e0503d6307e994c59760b7d8f76aa6
PJ Hyett <pjhyett@gmail.com>
2008-06-28 15:43:31 -0700

    minor tweaks to the email hook

--- services/email.rb
+++ services/email.rb
@@ -1,13 +1,15 @@
-# Inspired by: http://github.com/adamhjk/github-commit-email/tree
 service :email do |data, payload|
+
+  name_with_owner = File.join(payload['repository']['owner']['name'], payload['repository']['name'])
   
   body = <<-EOH
-Commits to #{payload['repository']['name']} (#{payload['repository']['url']})
+Commits to #{name_with_owner} (#{payload['repository']['url']})
 Ref: #{payload['ref']} 
 
 EOH
+
   payload['commits'].each do |gitsha, commit|
-  body << <<-EOH
+    body << <<-EOH
 #{gitsha} by #{commit['author']['name']} (#{commit['author']['email']}) @ #{commit['timestamp']}
 #{commit['url']}
 
@@ -17,12 +19,12 @@ EOH
   end
   
   message = TMail::Mail.new
-  message.set_content_type("text", "plain")
-  message.subject = "GitHub commit notice for #{payload['repository']['owner']['name']}-#{payload['repository']['name']}"
-  message.date = Time.now
-  message.body = body
+  message.set_content_type('text', 'plain')
+  message.subject = "[GitHub] Push: #{name_with_owner} - #{payload['ref'].split('/').last}"
+  message.body    = body
+  message.date    = Time.now
   
-  Net::SMTP.start('localhost', 25) do |smtp|
-    smtp.send_message message.to_s, "#{payload['repository']['owner']['name']}-#{payload['repository']['name']}@github.com", data['address']
+  Net::SMTP.start('localhost', 25, 'github.com') do |smtp|
+    smtp.send_message message.to_s, 'noreply@github.com', data['address']
   end
 end
