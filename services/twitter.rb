abc813995462801fbcd041d91e1d65df46294707
PJ Hyett <pjhyett@gmail.com>
2008-07-11 12:13:42 -0700

    need to define the variable outside of the begin block

--- services/twitter.rb
+++ services/twitter.rb
@@ -4,6 +4,7 @@ service :twitter do |data, payload|
 
   payload['commits'].each do |commit|
     commit = commit.last
+    github_url = nil
     begin
       Timeout::timeout(2) do
         github_url = Net::HTTP.get "is.gd", "/api.php?longurl=#{commit['url']}"
