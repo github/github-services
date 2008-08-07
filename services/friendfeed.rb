ee19fb2c440f1d0473597da7c815b5c0181208b8
PJ Hyett <pjhyett@gmail.com>
2008-08-07 16:14:07 -0700

    clean up code a bit

--- services/friendfeed.rb
+++ services/friendfeed.rb
@@ -1,21 +1,15 @@
 service :friendfeed do |data, payload|
-  # You need your friendfeed nickname and your friendfeed 
   repository = payload['repository']['name']
   friendfeed_url = URI.parse("http://friendfeed.com/api/share")
 
   payload['commits'].each do |commit|
-    commit_id = commit['id']
-    github_url, name, commit_msg = commit['url'], commit['author']['name'], commit['message']
-    title = "#{name} just pushed #{commit_id} to #{repository} on GitHub"
-    # This commit will be published to FriendFeed as 'Arun Thampi just pushed 56436bcdef2342ddfca234234 to github-services on GitHub'
-    # with the comment set as 'Integrated FriendFeed in github-services'
+    title = "#{commit['author']['name']} just pushed #{commit['id']} to #{repository} on GitHub"
     req = Net::HTTP::Post.new(friendfeed_url.path)
     req.basic_auth(data['nickname'], data['remotekey'])
-    req.set_form_data(
-      'title' => title, 'link' => github_url,
-      'comment' => commit_msg
-    )
+    req.set_form_data('title' => title, 'link' => commit['url'], 'comment' => commit['message'])
 
     Net::HTTP.new(friendfeed_url.host, friendfeed_url.port).start { |http| http.request(req) }
   end
 end
+
+genii904parch
