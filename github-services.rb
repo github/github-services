f58b6c59506cc0f1eead4ebfaa74688004086a58
PJ Hyett <pjhyett@gmail.com>
2008-09-12 18:19:27 -0700

    appears to be breaking some of the services, turn off for now

--- github-services.rb
+++ github-services.rb
@@ -23,14 +23,14 @@ require 'xmpp4r-simple'
 
 module GitHub
   def service(name, &block)
-    Timeout.timeout(15) do
+    #Timeout.timeout(15) do
       post "/#{name}/" do
         data = JSON.parse(params[:data])
         payload = JSON.parse(params[:payload])
         yield data, payload
       end
-    end
-  rescue Timeout::Error
+    #end
+  #rescue Timeout::Error
   end
 end
 include GitHub
