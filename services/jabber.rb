2e95078fb06a90c317726d047112c1b41bb76a62
Tom Preston-Werner <tom@mojombo.com>
2009-01-12 21:58:13 -0800

    force Jabber::Simple to send messages

--- services/jabber.rb
+++ services/jabber.rb
@@ -1,12 +1,23 @@
+# Jabber::Simple does some insane kind of queueing if it thinks
+# we are not in their buddy list (which is always) so messages
+# never get sent before we disconnect. This forces the library
+# to assume the recipient is a buddy.
+class Jabber::Contact
+  def subscribed_to?(x); true; end
+end
+
 service :jabber do |data, payload|
   repository = payload['repository']['name']
   branch     = payload['ref'].split('/').last
   recipient  = data['user']
   im         = Jabber::Simple.new(jabber_user, jabber_password)
-  
+
+  # Ask recipient to be our buddy if need be
+  im.add(recipient)
+
   # Accept any friend request
   im.accept_subscriptions = true
-  
+
   payload['commits'].each do |commit|
     sha1 = commit['id']
     im.deliver recipient, <<EOM
