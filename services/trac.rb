0fe12639346e976bc07d78a11d0d3704a2ac4f80
PJ Hyett <pjhyett@gmail.com>
2009-01-12 16:12:17 -0800

    couple trac tweaks

--- services/trac.rb
+++ services/trac.rb
@@ -1,11 +1,4 @@
 service :trac do |data, payload|
-    url = data['url']
-    # This is the API Token that is set in your trac.ini
-    api_token = data['apitoken']
-    # Build the URL
-    # This should build a URL like this:
-    #       http://yourserver.com/projects/myapp/github/APITOKEN
-    url = URI.join(url, "/github/", api_token)
-    # Send the request..
-    Net::HTTP.post_form(url, payload)
+  url = URI.join(data['url'], '/github/', data['token'])
+  Net::HTTP.post_form(url, payload)
 end
