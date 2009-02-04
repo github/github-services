6783f77de01bc2a21b10506f0de525bdcbb0d038
Dav Glass <davglass@gmail.com>
2009-02-05 00:09:06 +0800

    Fixed issue with subdirectories in the Trac URL
    Signed-off-by: PJ Hyett <pjhyett@gmail.com>

--- services/trac.rb
+++ services/trac.rb
@@ -1,4 +1,7 @@
 service :trac do |data, payload|
-  url = URI.join(data['url'], '/github/', data['token'])
-  Net::HTTP.post_form(url, payload)
+    #The data['url'] may contain a subdirectory, so this fails
+    #url = URI.join(data['url'], '/github/', data['token'])
+    url_value = data['url'].chomp('/')
+    url = "#{url_value}/github/#{data['token']}"
+    Net::HTTP.post_form(URI.parse(url), payload)
 end
