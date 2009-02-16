aa34d85d29688027b855d04e912a0dc32d3c22ef
Chris Wanstrath <chris@ozmm.org>
2009-02-16 13:36:55 -0800

    [campfire] Don't ignore remote merges as they're most likely pulls

--- services/campfire.rb
+++ services/campfire.rb
@@ -6,16 +6,16 @@ service :campfire do |data, payload|
   throw(:halt, 400) unless campfire && campfire.login(data['email'], data['password'])
   throw(:halt, 400) unless room = campfire.find_room_by_name(data['room'])
 
-  if payload['commits'].last && payload['commits'].last['message'] =~ /^Merge/
+  if payload['commits'].last && payload['commits'].last['message'] =~ /^Merge/ && payload['commits'].last['message'] !~ /\sof\s/
     commits = payload['commits'][-1, 1]
   else
     commits = payload['commits']
   end
-  
+
   commits.each do |commit|
     room.speak "[#{repository}/#{branch}] #{commit['message']} - #{commit['author']['name']} (#{commit['url']})"
   end
-  
+
   room.leave
   campfire.logout
 end
