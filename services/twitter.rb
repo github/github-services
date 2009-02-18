f6bc51820a9362b9c7009370901c7fa0de11c08a
jmkogut <joshua.kogut@gmail.com>
2009-02-18 15:32:28 +0800

    Changed twitter behaviour to have the ability to digest commits instead of post each individually
    Signed-off-by: Chris Wanstrath <chris@ozmm.org>

--- services/twitter.rb
+++ services/twitter.rb
@@ -1,11 +1,21 @@
 service :twitter do |data, payload|
   repository = payload['repository']['name']
-  url = URI.parse("http://twitter.com/statuses/update.xml")
+  url = URI.parse("http://twitter.com/statuses/update.xml")  
+  statuses = Array.new
 
-  payload['commits'].each do |commit|
-    tiny_url = shorten_url(commit['url'])
-    status   = "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{commit['message']}"
+  if data['digest'] == false then #=> continue normal behaviour
+    payload['commits'].each do |commit|
+      tiny_url = shorten_url(commit['url'])
+      statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{commit['message']}"
+    end
+  else #=> Only post the latest commit
+    payload['commits'].pop do |commit|
+      tiny_url = shorten_url(commit['url'])
+      statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{payload['commits'].length} commits"
+    end
+  end
 
+  statuses.each do |status|
     req = Net::HTTP::Post.new(url.path)
     req.basic_auth(data['username'], data['password'])
     req.set_form_data('status' => status, 'source' => 'github')
