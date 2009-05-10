3fcc4ffeb6f05b5ae87c0a99afbdc478c4458dde
bbhoss <preston@synergy-solutions.biz>
2009-05-10 18:59:15 +0800

    Added multiple-user support to the jabber plugin.
    Signed-off-by: PJ Hyett <pjhyett@gmail.com>

--- services/jabber.rb
+++ services/jabber.rb
@@ -9,24 +9,31 @@ end
 service :jabber do |data, payload|
   repository = payload['repository']['name']
   branch     = payload['ref'].split('/').last
-  recipient  = data['user']
   im         = Jabber::Simple.new(jabber_user, jabber_password)
 
-  # Ask recipient to be our buddy if need be
-  im.add(recipient)
 
   # Accept any friend request
   im.accept_subscriptions = true
 
-  payload['commits'].each do |commit|
-    sha1 = commit['id']
-    im.deliver recipient, <<EOM
+  #Split multiple addresses into array
+  recipients  = data['user'].split(',')
+
+  #Send message to each member in array 
+  recipients.each do |recipient|
+    # Ask recipient to be our buddy if need be
+    im.add(recipient)
+
+
+    payload['commits'].each do |commit|
+      sha1 = commit['id']
+      im.deliver recipient, <<EOM
 #{repository}: #{commit['author']['name']} #{branch} SHA1-#{sha1[0..6]}"
 
 #{commit['message']}
 #{commit['url']}
 EOM
   end
+    end
 
   im.disconnect
 end
