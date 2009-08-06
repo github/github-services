628a042982cfbef39589736baac7814e39d786f7
tekkub <tekkub@gmail.com>
2009-08-05 22:41:18 -0600

    Add spam protection to campfire service

--- services/campfire.rb
+++ services/campfire.rb
@@ -1,19 +1,20 @@
 service :campfire do |data, payload|
   repository = payload['repository']['name']
   branch     = payload['ref'].split('/').last
+  commits    = payload['commits']
   campfire   = Tinder::Campfire.new(data['subdomain'], :ssl => data['ssl'].to_i == 1)
 
   throw(:halt, 400) unless campfire && campfire.login(data['email'], data['password'])
   throw(:halt, 400) unless room = campfire.find_room_by_name(data['room'])
 
-  if payload['commits'].last && payload['commits'].last['message'] =~ /^Merge/ && payload['commits'].last['message'] !~ /\sof\s/
-    commits = payload['commits'][-1, 1]
-  else
-    commits = payload['commits']
-  end
 
-  commits.each do |commit|
-    room.speak "[#{repository}/#{branch}] #{commit['message']} - #{commit['author']['name']} (#{commit['url']})"
+  if commits.size > 5
+    commit = commits[payload['after']]
+    room.speak "[#{repository}/#{branch}] #{commit['message']} (+#{commits.size - 1} more commits...) - #{commit['author']['name']} (#{commit['url']})"
+  else
+    commits.each do |commit|
+      room.speak "[#{repository}/#{branch}] #{commit['message']} - #{commit['author']['name']} (#{commit['url']})"
+    end
   end
 
   room.leave
