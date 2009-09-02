7fb1dd3857269fd3252bdfe10b13d518e04935cd
Larry Karnowski <larry@thinkrelevance.com>
2009-09-02 18:08:13 -0400

    Added a script that creates the correct Curl command to test your local github-service.  Had to change the marshalling of the payload for RunCodeRun.

--- services/runcoderun.rb
+++ services/runcoderun.rb
@@ -1,4 +1,5 @@
 service :runcoderun do |data, payload|
   runcoderun_url = URI.parse("http://runcoderun.com/github")
-  Net::HTTP.post_form(runcoderun_url, payload)
+  Net::HTTP.post_form(runcoderun_url, :payload => JSON.generate(payload))
+  "this output here to make the Sinatra happy when testing a GitHub service, biznatch!"
 end
