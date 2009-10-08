ef10cbf51c4e9fcd6df1ce743180299e9fbec4f3
Chris Wanstrath <chris@ozmm.org>
2009-10-08 16:40:06 -0700

    remove old rcr files

--- services/runcoderun.rb
+++
@@ -1,5 +0,0 @@
-service :runcoderun do |data, payload|
-  runcoderun_url = URI.parse("http://runcoderun.com/github")
-  Net::HTTP.post_form(runcoderun_url, :payload => JSON.generate(payload))
-  "this output here to make the Sinatra happy when testing a GitHub service, biznatch!"
-end
