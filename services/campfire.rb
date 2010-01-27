0f83b6adac1d70489cb19fff74008c8d974f78c4
Ryan Tomayko <rtomayko@gmail.com>
2010-01-26 23:01:24 -0800

    Revert "retry campfire find room requests due to weird conn errors"
    This reverts commit 512b03cf284e4c7fc855f9a6743a5a7e3da14822.

--- services/campfire.rb
+++ services/campfire.rb
@@ -1,4 +1,3 @@
-
 service :campfire do |data, payload|
   repository = payload['repository']['name']
   branch     = payload['ref'].split('/').last
@@ -7,20 +6,7 @@ service :campfire do |data, payload|
   play_sound = data['play_sound'].to_i == 1
 
   throw(:halt, 400) unless campfire && campfire.login(data['token'], 'X')
-
-  # XXX temporary band-aid to work around intermittent errors locating
-  # a campfire room.
-  attempts = 0
-  begin
-    room = campfire.find_room_by_name(data['room'])
-  rescue NoMethodError
-    attempts += 1
-    $stderr.puts "retrying failed find room attempt #{attempts}"
-    retry if attempts <= 3
-    raise
-  end
-
-  throw(:halt, 400) unless room
+  throw(:halt, 400) unless room = campfire.find_room_by_name(data['room'])
 
   message  = "[#{repository}/#{branch}]\n"
   message += commits.map do |commit|
