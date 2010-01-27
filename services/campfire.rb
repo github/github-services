a06b9b78bbe21f27d080e2ec2ba87f4e78c71631
Chris Wanstrath <chris@ozmm.org>
2010-01-26 20:58:52 -0800

    pj approves

--- services/campfire.rb
+++ services/campfire.rb
@@ -8,7 +8,7 @@ service :campfire do |data, payload|
   throw(:halt, 400) unless campfire && campfire.login(data['token'], 'X')
   throw(:halt, 400) unless room = campfire.find_room_by_name(data['room'])
 
-  if commits.size > 10
+  if commits.size > 1
     commit = commits.last
     before, after = payload['before'], payload['after']
     compare_url = payload['repository']['url'] + "/compare/#{before}...#{after}"
