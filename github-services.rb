06b5614ab6e117fb377b5512b0ec420d4d3bdc98
Ryan Tomayko <rtomayko@gmail.com>
2010-01-27 02:04:24 -0800

    helps when you define variables before use

--- github-services.rb
+++ github-services.rb
@@ -57,13 +57,14 @@ module GitHub
       rescue Object => boom
         # redact sensitive info in hook_data hash
         hook_data = data || params[:data]
+        hook_payload = payload || params[:payload]
         %w[password token].each { |key| hook_data[key] &&= '<redacted>' }
         owner = hook_payload['repository']['owner']['name'] rescue nil
         repo  = hook_payload['repository']['name'] rescue nil
         report_exception boom,
           :hook_name    => name,
           :hook_data    => hook_data.inspect,
-          :hook_payload => (payload || params[:payload]).inspect,
+          :hook_payload => hook_payload.inspect,
           :user         => owner,
           :repo         => "#{owner}/#{repo}"
         raise
