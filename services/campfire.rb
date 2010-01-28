9d56d2904155ae560c19c60323fb333b04ec1c7c
Ryan Tomayko <rtomayko@gmail.com>
2010-01-28 14:03:38 -0800

    campfire formatting tweaks

--- services/campfire.rb
+++ services/campfire.rb
@@ -13,22 +13,31 @@ service :campfire do |data, payload|
   throw(:halt, 400) unless campfire && campfire.login(data['token'], 'X')
   throw(:halt, 400) unless room = campfire.find_room_by_name(data['room'])
 
-  message  = "[#{owner}/#{repository}/#{branch}]\n"
-  message += commits.map do |commit|
-    short = commit['message'].split("\n", 2).first
-    short += ' ...' if short != commit['message']
-    "#{commit['id'][0..6]} #{short} - #{commit['author']['name']}"
-  end.join("\n")
+  prefix = "[#{repository}/#{branch}]"
+  primary, others = commits[0..4], Array(commits[5..-1])
+  messages =
+    primary.map do |commit|
+      short = commit['message'].split("\n", 2).first
+      short += ' ...' if short != commit['message']
+      "#{prefix} #{short} - #{commit['author']['name']}"
+    end
 
-  if commits.size > 1
+  if messages.size > 1
     before, after = payload['before'][0..6], payload['after'][0..6]
     url = payload['repository']['url'] + "/compare/#{before}...#{after}"
+    summary =
+      if others.any?
+        "#{prefix} (+#{others.length} more) commits #{before}...#{after}: #{url}"
+      else
+        "#{prefix} commits #{before}...#{after}: #{url}"
+      end
+    messages << summary
   else
     url = commits.first['url']
+    messages[0] = "#{messages.first} (#{url})"
   end
 
-  room.paste message
-  room.speak url
+  messages.each { |line| room.speak line }
   room.play "rimshot" if play_sound
 
   room.leave
