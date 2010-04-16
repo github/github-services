1ac3cc996a8623f9f33baf90416656066529f6f8
Garret Heaton <powdahound@gmail.com>
2010-04-16 11:31:39 +0800

    Add HipChat as GitHub service.

---
+++ services/hipchat.rb
@@ -0,0 +1,16 @@
+service :hipchat do |data, payload|
+  # make sure we have what we need
+  throw :halt, [400, "Missing auth_token"] if data['auth_token'].to_s == ''
+  throw :halt, [400, "Missing room"] if data['room'].to_s == ''
+
+  # send it
+  url = URI.parse("http://api.hipchat.com/v1/rooms/message_github")
+  Net::HTTP.post_form(url, {
+    :auth_token => data['auth_token'],
+    :room_id => data['room'],
+    :payload => JSON.generate(payload),
+  })
+
+  # Sinatra expects a string return
+  "Great success!"
+end
