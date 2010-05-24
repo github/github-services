6bc92064d41a9c4fa6ad8ff6a7c459ded2a6c6b3
rick <technoweenie@gmail.com>
2010-05-24 13:07:09 -0700

    merge jaconda patch

---
+++ services/jaconda.rb
@@ -0,0 +1,32 @@
+service :jaconda do |data, payload|
+  throw(:halt, 400) if data['api_token'].to_s == '' || data['room_id'].to_s == ''
+  
+  repository = CGI.escapeHTML(payload['repository']['name'])
+  branch = CGI.escapeHTML(payload['ref'].split('/').last)
+  commits = payload['commits']
+  api_token = data['api_token']
+  room_id = data['room_id']
+  url = URI.parse("http://#{api_token}:X@jaconda.im/api/rooms/#{room_id}/messages.json")
+  
+  before, after = payload['before'][0..6], payload['after'][0..6]
+  compare_url = payload['repository']['url'] + "/compare/#{before}...#{after}"
+  
+  if data['digest'].to_i == 1 && commits.size > 1
+    commit = commits.first
+    message = "<i>Commit <a href='#{commit["url"]}'>#{commit["id"][0..6]}</a> on #{repository}/#{branch} by #{CGI.escapeHTML(commit["author"]["name"])}</i><br /><br />"
+    message += CGI.escapeHTML(commit["message"])
+    message += "<br /><br /><i>(+#{commits.size - 1} <a href='#{compare_url}'>more commits</a>)</i>"
+    
+    res = Net::HTTP.post_form(url, {:text => message})
+    throw(:halt, [res.code.to_i, res.body.to_s]) unless res.is_a?(Net::HTTPSuccess)
+  else
+    commits.each do |commit|
+      message = "<i>Commit <a href='#{commit["url"]}'>#{commit["id"][0..6]}</a> on #{repository}/#{branch} by #{CGI.escapeHTML(commit["author"]["name"])}</i><br /><br />"
+      message += CGI.escapeHTML(commit["message"])
+      
+      res = Net::HTTP.post_form(url, {:text => message})
+      throw(:halt, [res.code.to_i, res.body.to_s]) unless res.is_a?(Net::HTTPSuccess)
+    end
+  end
+  true
+end
