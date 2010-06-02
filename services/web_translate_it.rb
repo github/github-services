5824aca7c77ddc2c676d65ffa6a3e052e8595c45
Ryan Tomayko <rtomayko@gmail.com>
2010-06-02 10:43:26 -0700

    web_translate_it raises service config error on bad response

--- services/web_translate_it.rb
+++ services/web_translate_it.rb
@@ -1,4 +1,8 @@
 service :web_translate_it do |data, payload|
-  wti_url = URI.parse("https://webtranslateit.com/api/projects/#{data['api_key']}/refresh_files")
-  Net::HTTP.post_form(wti_url, :payload => JSON.generate(payload))
+  begin
+    wti_url = URI.parse("https://webtranslateit.com/api/projects/#{data['api_key']}/refresh_files")
+    Net::HTTP.post_form(wti_url, :payload => JSON.generate(payload))
+  rescue Net::HTTPBadResponse
+    raise GitHub::ServiceConfigurationError, "Invalid configuration"
+  end
 end
