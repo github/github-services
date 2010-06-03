50f8d152e580c7506d6ec55c37ef76cb1cd369fd
rick <technoweenie@gmail.com>
2010-06-03 10:47:34 -0700

    irc hook needs to just _chill_

--- services/irc.rb
+++ services/irc.rb
@@ -12,25 +12,25 @@ service :irc do |data, payload|
 
     messages =
       payload['commits'].map do |commit|
-        short = commit['message'].split("\n", 2).first
+        short  = commit['message'].split("\n", 2).first.to_s
         short += ' ...' if short != commit['message']
         author = commit['author']['name']
-        sha1 = commit['id']
-        files = Array(commit['modified'])
-        dirs = files.map { |file| File.dirname(file) }.uniq
+        sha1   = commit['id']
+        files  = Array(commit['modified'])
+        dirs   = files.map { |file| File.dirname(file) }.uniq
         "\002#{repository}:\002 \00307#{branch}\003 \00303#{author}\003 * " +
         "\002#{sha1[0..6]}\002 (#{files.size} files in #{dirs.size} dirs): #{short}"
       end
 
     if messages.size > 1
       before, after = payload['before'][0..6], payload['after'][0..6]
-      compare_url = payload['repository']['url'] + "/compare/#{before}...#{after}"
-      tiny_url = shorten_url(compare_url)
-      summary = "\002#{repository}:\002 \00307#{branch}\003 commits " +
+      compare_url   = payload['repository']['url'] + "/compare/#{before}...#{after}"
+      tiny_url      = shorten_url(compare_url)
+      summary       = "\002#{repository}:\002 \00307#{branch}\003 commits " +
                 "\002#{before}\002...\002#{after}\002 - #{tiny_url}"
       messages << summary
     else
-      commit = payload['commits'][0]
+      commit   = payload['commits'][0]
       tiny_url = shorten_url(commit['url'])
       messages[0] << " - #{tiny_url}"
     end
