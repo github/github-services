c870aeaa5b847df2758ecc5b01d587b949038576
Ryan Tomayko <rtomayko@gmail.com>
2010-10-29 02:00:41 -0700

    statusnet raises config error when connection is refused

--- services/statusnet.rb
+++ services/statusnet.rb
@@ -1,24 +1,28 @@
 service :statusnet do |data, payload|
-  repository = payload['repository']['name']
-  url = URI.parse(data['server'] + "/api/statuses/update.xml")
-  statuses = Array.new
+  begin
+    repository = payload['repository']['name']
+    url = URI.parse(data['server'] + "/api/statuses/update.xml")
+    statuses = Array.new
 
-  if data['digest'] == '1'
-    commit = payload['commits'][-1]
-    tiny_url = shorten_url(payload['repository']['url'] + '/commits/' + payload['ref_name'])
-    statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{payload['commits'].length} commits"
-  else
-    payload['commits'].each do |commit|
-      tiny_url = shorten_url(commit['url'])
-      statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{commit['message']}"
+    if data['digest'] == '1'
+      commit = payload['commits'][-1]
+      tiny_url = shorten_url(payload['repository']['url'] + '/commits/' + payload['ref_name'])
+      statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{payload['commits'].length} commits"
+    else
+      payload['commits'].each do |commit|
+        tiny_url = shorten_url(commit['url'])
+        statuses.push "[#{repository}] #{tiny_url} #{commit['author']['name']} - #{commit['message']}"
+      end
     end
-  end
 
-  statuses.each do |status|
-    req = Net::HTTP::Post.new(url.path)
-    req.basic_auth(data['username'], data['password'])
-    req.set_form_data('status' => status, 'source' => 'github')
+    statuses.each do |status|
+      req = Net::HTTP::Post.new(url.path)
+      req.basic_auth(data['username'], data['password'])
+      req.set_form_data('status' => status, 'source' => 'github')
 
-    Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
+      Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
+    end
+  rescue Errno::ECONNREFUSED => boom
+    raise GitHub::ServiceConfigurationError, "Connection refused. Invalid server configuration."
   end
 end
