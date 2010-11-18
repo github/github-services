032ec45b0a2c4765ab7ab12aabdaf4abe00cf3f3
rick <technoweenie@gmail.com>
2010-11-18 14:33:18 -0800

    rename ContinuityApp hook to fit naming convention

--- services/continuity_app.rb
+++ services/continuity_app.rb
@@ -1,4 +1,4 @@
-service :continuityapp do |data, payload|
+service :continuity_app do |data, payload|
   hook_url = URI.parse("https://hooks.continuityapp.com/github_selfservice/v1/%d" % data['project_id'].to_i)
   Net::HTTP.post_form(hook_url, :payload => JSON.generate(payload))
 end
