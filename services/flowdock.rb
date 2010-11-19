ce2b0f066c32ad270b77279e88a4e79773b63188
rick <technoweenie@gmail.com>
2010-11-19 10:09:24 -0800

    flowdock service hook should use the same token variable that's documented

--- services/flowdock.rb
+++ services/flowdock.rb
@@ -1,9 +1,9 @@
 service :flowdock do |data, payload|
-  raise GitHub::ServiceConfigurationError, "Missing token" if data['api_token'].to_s.empty?
+  raise GitHub::ServiceConfigurationError, "Missing token" if data['token'].to_s.empty?
 
   url = URI.parse("http://api.flowdock.com/v1/git")
   Net::HTTP.post_form(url, {
-    :token => data['api_token'],
+    :token => data['token'],
     :payload => JSON.generate(payload),
   })
 end
