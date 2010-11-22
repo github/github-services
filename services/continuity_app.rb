e6e0c8d9d3af4f59da8462d26363b9926b203958
rick <technoweenie@gmail.com>
2010-11-22 00:20:05 -0500

    apparently https doesnt work

--- services/continuity_app.rb
+++ services/continuity_app.rb
@@ -1,4 +1,4 @@
 service :continuity_app do |data, payload|
-  hook_url = URI.parse("https://hooks.continuityapp.com/github_selfservice/v1/%d" % data['project_id'].to_i)
+  hook_url = URI.parse("http://hooks.continuityapp.com/github_selfservice/v1/%d" % data['project_id'].to_i)
   Net::HTTP.post_form(hook_url, :payload => JSON.generate(payload))
 end
