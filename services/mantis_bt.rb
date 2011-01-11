0beb4868dd8e08285c473890ab6257d753d83595
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-01-11 12:43:01 -0800

    Added API Key to Mantis_BT as an alternative to Remote IP check

--- services/mantis_bt.rb
+++ services/mantis_bt.rb
@@ -2,6 +2,7 @@ service :mantis_bt do |data, payload|
   begin
     url_value = data['url'].chomp('/')
     url = "#{url_value}/plugin.php?page=Source/checkin"
+	payload['api_key'] = data['api_key']
     Net::HTTP.post_form(URI.parse(url), "payload" => payload.to_json)
   rescue Net::HTTPBadResponse => boom
     raise GitHub::ServiceConfigurationError, "Invalid configuration"
