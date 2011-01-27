75a368c1a4dad6b68d903c3649d6a4bb17f6ed6e
rick <technoweenie@gmail.com>
2011-01-27 11:52:32 -0800

    conventions are liberating

--- services/read_the_docs.rb
+++ services/read_the_docs.rb
@@ -1,4 +1,4 @@
-service :readthedocs do |data, payload|
+service :read_the_docs do |data, payload|
   Net::HTTP.post_form(URI.parse("http://readthedocs.org/github"), :payload => JSON.generate(payload))
 end
 
