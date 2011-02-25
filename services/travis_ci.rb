13dd823fad380d71651143d56c5e5344fece4298
Matthias Viehweger <kronn@kronn.de>
2011-02-25 08:24:43 +0100

    add service hook for Travis (travis-ci.org)

---
+++ services/travis_ci.rb
@@ -0,0 +1,9 @@
+service :travis_ci do |data, payload|
+  user = payload['repository']['owner']['name']
+  token = data['token']
+
+  travis_url = URI.parse("http://#{user}:#{token}@travis-ci.org/builds")
+
+  Net::HTTP.post_form(travis_url, :payload => JSON.generate(payload))
+  nil
+end
