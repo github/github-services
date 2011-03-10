aa60399c24edd2e566e969375343c39c3aef4eb9
rick <technoweenie@gmail.com>
2011-03-10 13:44:06 -0800

    script/bootstrap

--- github-services.rb
+++ github-services.rb
@@ -1,21 +1,4 @@
-$LOAD_PATH.unshift *Dir["#{File.dirname(__FILE__)}/vendor/**/lib"]
-
-# stdlib
-require 'net/http'
-require 'net/https'
-require 'net/smtp'
-require 'socket'
-require 'xmlrpc/client'
-require 'openssl'
-require 'cgi'
-#~ require 'date' # This is needed by the CIA service in ruby 1.8.7 or later
-
-# vendor
-require 'rubygems'
-require 'bundler'
-Bundler.require
-require 'basecamp'
-require 'rubyforge'
+require File.expand_path('../config/load', __FILE__)
 
 set :run, true
 set :environment, :production
