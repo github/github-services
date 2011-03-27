d910626a69b9dd4b1bd4132400d06f891aadb6e4
Petteri Muilu <petteri@synble.com>
2011-03-27 13:56:55 +0300

    Services hooks for GetLocalization.com

---
+++ services/get_localization.rb
@@ -0,0 +1,13 @@
+service :get_localization do |data, payload|
+  project_name = data['project_id']
+  project_secret = data['project_token']
+  
+  gl_uri = "https://www.getlocalization.com/services/github/notify/#{project_name}/#{project_token}/"
+  
+  begin
+    url = URI.parse(gl_uri)
+    Net::HTTP.post_form(url, :payload => JSON.generate(payload))
+  rescue Net::HTTPBadResponse
+    raise GitHub::ServiceConfigurationError, "Invalid configuration"
+  end
+end
