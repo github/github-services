aed13da3a86e75a4ca8745881cba0ea7a28851b6
Matthias Viehweger <kronn@kronn.de>
2011-04-10 15:38:39 +0200

    rewrite if/else so that it is easier to read

--- services/travis.rb
+++ services/travis.rb
@@ -1,10 +1,19 @@
 service :travis do |data, payload|
-  user = ((data['user'].to_s == '') ? payload['repository']['owner']['name'] : data['user']).strip
+  user = if data['user'].to_s == ''
+           payload['repository']['owner']['name']
+         else
+           data['user']
+         end.strip
+
   token = data['token'].strip
 
-  full_domain = ((data['domain'].to_s == '') ? 'http://travis-ci.org' : data['domain']).strip
+  full_domain = if data['domain'].to_s == ''
+                  'http://travis-ci.org'
+                else
+                  data['domain']
+                end.strip
 
-  scheme, domain = full_domain.strip.split('://')
+  scheme, domain = full_domain.split('://')
   scheme, domain = 'http', scheme if domain.nil?
 
   travis_url = URI.parse("#{scheme}://#{user}:#{token}@#{domain}/builds")
