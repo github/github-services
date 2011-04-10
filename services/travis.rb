2e504eeb24e4b831d302aec8455cf25bca3c1b9c
Matthias Viehweger <kronn@kronn.de>
2011-04-10 15:31:17 +0200

    set the default domain if none is provided

--- services/travis.rb
+++ services/travis.rb
@@ -1,8 +1,10 @@
 service :travis do |data, payload|
   user = ((data['user'].to_s == '') ? payload['repository']['owner']['name'] : data['user']).strip
   token = data['token'].strip
-  domain = ((data['domain'].to_s == '') ? 'http://travis-ci.org' : data['domain']).strip
-  scheme, domain = data['domain'].strip.split '://'
+
+  full_domain = ((data['domain'].to_s == '') ? 'http://travis-ci.org' : data['domain']).strip
+
+  scheme, domain = full_domain.strip.split('://')
   scheme, domain = 'http', scheme if domain.nil?
 
   travis_url = URI.parse("#{scheme}://#{user}:#{token}@#{domain}/builds")
