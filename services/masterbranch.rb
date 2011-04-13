f06e93d3f09bbeb828d44d4a5eea7a5ea524a89a
Pablo Molina <faival@gmail.com>
2011-04-13 11:51:55 +0200

    service clean and ready for pull request

--- services/masterbranch.rb
+++ services/masterbranch.rb
@@ -1,12 +1,12 @@
 service :masterbranch do |data,payload|
 	# fail fast with no token
 	
-	url = URI.parse("http://192.168.1.33:9003/gh-hook")
+	url = URI.parse("http://webhooks.masterbranch.com/gh-hook")
 	
 	req = Net::HTTP::Post.new(url.path)
 	
 	req.set_form_data({'payload' => JSON.generate(payload)})
-	puts "sending a commit"
+	
 	http = Net::HTTP.new(url.host, url.port)
 	http.use_ssl = true if url.port == 443 || url.instance_of?(URI::HTTPS)
 	http.start { |http| http.request(req)}			
