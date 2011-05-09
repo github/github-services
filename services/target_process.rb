66442ba722fa9679a49fce6fad5c17030a2de978
Jonathan Enzinna <jonathan@jiffsoft.com>
2011-05-09 09:42:05 -0400

    Changed class to use REST API for read-only tasks

--- services/target_process.rb
+++ services/target_process.rb
@@ -1,17 +1,56 @@
 module TargetProcess
+  class Users
+    include HTTParty
+    format :xml
+
+    def initialize(u, p, b)
+      @auth = {:username => u, :password => p}
+      self.base_uri = b
+    end
+
+    def gather_all(acid)
+      self.class.get('/api/v1/Users/?acid='+acid,{:basic_auth => @auth})
+    end
+  end
+
+  class Context
+    include HTTParty
+    format :xml
+
+    def initialize(u, p, b)
+      @auth = {:username => u, :password => p}
+      self.base_uri = b
+    end
+
+    def get_by_project(id)
+      self.class.get('/api/v1/Context?ids='+id,{:basic_auth => @auth})
+    end
+  end
+
+  class EntityState
+    include HTTParty
+    format :xml
+
+    def initialize(u, p, b)
+      @auth = {:username => u, :password => p}
+      self.base_uri = b
+    end
+
+    def gather_all(acid)
+      self.class.get('/api/v1/EntityState?acid='+acid,{:basic_auth => @auth})
+    end
+  end
+
   class Remote
     def initialize(data={})
       # required fot connection
-      @base_url, @username, @password, @project_key = data['base_url'], data['username'], data['password'], data['project_key']
+      @base_url, @username, @password, @project_id = data['base_url'], data['username'], data['password'], data['project_id']
       # check if all the variables are initialized from service params
-      [@base_url, @username, @password, @project_key].each{|var| raise GitHub::ServiceConfigurationError.new("Missing configuration: #{var}") if var.to_s.empty? }
+      [@base_url, @username, @password, @project_id].each{|var| raise GitHub::ServiceConfigurationError.new("Missing configuration: #{var}") if var.to_s.empty? }
       # delete last slash in the string
       correct_uri = @base_url.gsub(/\/$/, '')
       @uri = URI.parse(correct_uri)
-      @soap_path = @uri.path + "/Services/"
-      @tp_client = Savon::Client.new do
-        wsse.credentials @username, @password
-      end
+      get_context_and_users
     end
 
     def process_commits(payload)
@@ -26,43 +65,33 @@ private
       author = find_user_by_email(commit["author"]["email"])
       return if author.nil?
       commit["message"].each{ |commit_line|
-        bug_id = commit_line[/( |^)#(\w+-\d+) /, 2]
+        bug_id = commit_line[/( |^)#(\d+):.+/, 1]
         next if bug_id.nil?
-        command = commit_line[/( |^)#\w+-\d+ (.+)/, 2].strip
+        command = commit_line[/( |^)#\d+:(.+)/, 1].strip
         next if command.nil?
         get_state_data
         execute_command(author, bug_id, command, commit)
       }
     end
 
-    def get_state_data()
-      @tp_client.wsdl.document = @soap_path + 'ProjectService.asmx?WSDL'
-      response = @tp_client.request :wsdl, "Retreive", :hql => ["from Projects as p where p.IsActive = 1 and p.Abbreviation = '%s'" % @project_key]
-      raise GitHub::ServiceConfigurationError.new("Cannot find project #{@project_key}, verify it is active and exists") if !response.html.success?
-      process_id = response.to_hash.RetreiveResult.ProjectDTO.ProcessId
-      @tp_client.wsdl.document = @soap_path + 'ProcessService.asmx?WSDL'
-      response = @tp_client.request :wsdl, "RetrieveEntityStatesForProcess", :processID => process_id
-      raise GitHub::ServiceConfigurationError.new("Cannot retreive state data for process ID #{process_id}") if !response.html.success?
-      @state_data = response.to_hash
-    end
-
     def execute_command(author, bug_id, command, commit)
-      return if bug = find_bug_by_id(bug_id) == false
-      @state_data.RetrieveEntityStatesForProcessResult.each do |e|
+      @states.each do |e|
         next if e.Name != command
-        @tp_client.wsdl.document = @soap_path + 'BugService.asmx?WSDL'
-        response = @tp_client.request :update do
+        @soap_client = Savon::Client.new(@uri.path + "/Services/BugService.asmx?WSDL") do
+          wsse.credentials @username, @password
+        end
+        response = @soap_client.request :update do
           soap.body = {
-            :BugID => bug.BugID,
+            :BugID => bug_id,
             :LastEditorId => author.UserID,
-            :EntityStateID => e.EntityStateID
+            :EntityStateID => e.Id
           }
         end
         if response.html.success?
           # Add the comment
-          @tp_client.request :AddCommentToBug do
+          @soap_client.request :AddCommentToBug do
             soap.body = {
-              :BugID => bug.BugID,
+              :BugID => bug_id,
               :OwnerID => author.UserID,
               :Description => commit["Message"]
             }
@@ -72,15 +101,27 @@ private
     end
 
     def find_user_by_email(email)
-      @tp_client.wsdl.document = @soap_path + 'UserService.asmx?WSDL'
-      response = @tp_client.request :wsdl, :retrieve, :hql => ["from Users where active = 1 and email = '%s'" % email]
-      nil if !response.http.success? else response.to_hash.RetreiveResult.UserDTO rescue nil
+      @users.each do |u|
+        if u.Email == email
+          return u.Id
+        end
+      end
+      nil
     end
 
-    def find_bug_by_id(bug_id)
-      @tp_client.wsdl.document = @soap_path + 'BugService.asmx?WSDL'
-      response = @tp_client.request :wsdl, "GetByID", :id => bug_id
-      false if !response.http.success? else response.to_hash rescue false
+    def get_context_and_users()
+      @context_data = Context.new(@username,@password,@base_url).get_by_project(@project_id)
+      @users = Users.new(@username,@password,@base_url).gather_all(@context_data.Acid)
+    end
+
+    def get_state_data()
+      states = EntityState.new(@username,@password,@base_url).gather_all(@context_data.Acid)
+      @states = {}
+      states.each do |s|
+        if s.EntityType == "Bug"
+          @states.merge(s)
+        end
+      end
     end
   end
 end
