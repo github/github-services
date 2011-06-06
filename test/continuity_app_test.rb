269a611c5a1c5794439689ae4f52afd779c36a6b
rick <technoweenie@gmail.com>
2011-06-06 16:57:47 -0700

    port continuity app service

---
+++ test/continuity_app_test.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../helper', __FILE__)
+
+class ContinuityAppTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    svc = service :push, {'project_id' => '55'}, {'a' => 1}
+
+    @stubs.post "/github_selfservice/v1/55" do |env|
+      assert_match /(^|\&)payload=%7B%22a%22%3A1%7D($|\&)/, env[:body]
+      [200, {}, '']
+    end
+
+    svc.receive_push
+
+    @stubs.verify_stubbed_calls
+  end
+
+  def service(*args)
+    super Service::ContinuityApp, *args
+  end
+end
+
+
+
