cd3ca9145af27f980ebd04c7e914075c8a0cb377
rick <technoweenie@gmail.com>
2011-06-06 16:26:46 -0700

    port Bugly service

---
+++ test/bugly_test.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../helper', __FILE__)
+
+class BuglyTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    svc = service :push, {'token' => 'abc'}, {'a' => 1}
+
+    @stubs.post "/changesets.json" do |env|
+      assert_equal %({"a":1}), env[:body]
+      assert_equal 'abc', env[:request_headers]['X-BuglyToken']
+      assert_equal 'application/json', env[:request_headers]['Content-Type']
+      [200, {}, '']
+    end
+
+    svc.receive_push
+
+    @stubs.verify_stubbed_calls
+  end
+
+  def service(*args)
+    super Service::Bugly, *args
+  end
+end
+
+
+
