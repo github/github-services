2ed71e9a5b4f4391510bc714805faf4646585bbc
Fabian Schlenz <mail@fabianonline.de>
2011-06-08 14:21:33 +0200

    Added redmine service.

---
+++ services/redmine.rb
@@ -0,0 +1,15 @@
+service :redmine do |data, payload|
+    url = URI.join(data['address'], "sys/fetch_changesets?id=#{URI.escape(data['project'])}")
+    if (url.scheme == "http")
+        Net::HTTP.get(url)
+    elsif (url.scheme == "https")
+        # HTTPS
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        request = Net::HTTP::Get.new(url.request_uri)
+        http.start{|http| http.request(request)}
+    else
+        # do nothing
+    end 
+end
+
