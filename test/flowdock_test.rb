96c4635250d9a0d189bdbae18922731b6f8ef42f
rick <technoweenie@gmail.com>
2011-06-08 14:03:11 -0700

    port flowdock hook

---
+++ test/flowdock_test.rb
@@ -0,0 +1,26 @@
+require File.expand_path('../helper', __FILE__)
+
+class FlowdockTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    @stubs.post "/v1/git" do |env|
+      assert_match /(^|\&)payload=%7B%22a%22%3A1%7D($|\&)/, env[:body]
+      assert_match "token=t", env[:body]
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'token' => 't'}, {'a' => 1}
+    svc.receive_push
+  end
+
+  def service(*args)
+    super Service::Flowdock, *args
+  end
+end
+
+
+
