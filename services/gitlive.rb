c484aba42d31b394b97f460b2540090c4ac0aa8a
rick <technoweenie@gmail.com>
2011-06-08 15:37:53 -0700

    port gitlive hook

--- services/gitlive.rb
+++ services/gitlive.rb
@@ -1,5 +1,8 @@
-service :gitlive do |data, payload|
-  provider_url = URI.parse("http://gitlive.com/hook")
-  Net::HTTP.post_form(provider_url, 
-    {:payload => JSON.generate(payload)})
+class Service::GitLive < Service
+  self.hook_name = :gitlive
+
+  def receive_push
+    http_post 'http://gitlive.com/hook',
+      :payload => JSON.generate(payload)
+  end
 end
