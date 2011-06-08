e9408ff6de4e4c5fc30101d77f2e1bb116cc9ca1
rick <technoweenie@gmail.com>
2011-06-08 15:05:03 -0700

    port freckle service

--- services/freckle.rb
+++ services/freckle.rb
@@ -1,23 +1,25 @@
-service :freckle do |data, payload|
+class Service::Freckle < Service
+  self.hook_name = :freckle
 
-  entries, subdomain, token, project =
-    [], data['subdomain'].strip, data['token'].strip, data['project'].strip
+  def receive_push
+    entries, subdomain, token, project =
+      [], data['subdomain'].strip, data['token'].strip, data['project'].strip
 
-  payload['commits'].each do |commit|
-    minutes = (commit["message"].split(/\s/).find { |item| /^f:/ =~ item } || '')[2,100]
-    next unless minutes
-    entries << {
-      :date => commit["timestamp"],
-      :minutes => minutes,
-      :description => commit["message"].gsub(/(\s|^)f:.*(\s|$)/, '').strip,
-      :url => commit['url'],
-      :project_name => project,
-      :user => commit['author']['email']
-    }
+    payload['commits'].each do |commit|
+      minutes = (commit["message"].split(/\s/).find { |item| /^f:/ =~ item } || '')[2,100]
+      next unless minutes
+      entries << {
+        :date => commit["timestamp"],
+        :minutes => minutes,
+        :description => commit["message"].gsub(/(\s|^)f:.*(\s|$)/, '').strip,
+        :url => commit['url'],
+        :project_name => project,
+        :user => commit['author']['email']
+      }
+    end
+
+    http.headers['Content-Type'] = 'application/json'
+    http_post "http://#{data['subdomain']}.letsfreckle.com/api/entries/import",
+      {:entries => entries, :token => data['token']}.to_json
   end
-  uri = URI.parse("http://#{data['subdomain']}.letsfreckle.com/api/entries/import")
-  req = Net::HTTP::Post.new(uri.path)
-  req.set_content_type('application/json')
-  req.body = { :entries => entries, :token => data['token'] }.to_json
-  Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
 end
