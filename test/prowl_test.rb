2c178101c0dcfdcfd9e9c969adcf7acec4c11a8a
rick <technoweenie@gmail.com>
2011-06-09 10:03:28 -0700

    port prowl hook

---
+++ test/prowl_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path('../helper', __FILE__)
+
+class ProwlTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    @stubs.post "/publicapi/add" do |env|
+      assert_equal 'api.prowlapp.com', env[:url].host
+      data = Rack::Utils.parse_query(env[:body])
+      assert_equal 'a', data['apikey']
+      [200, {}, '']
+    end
+
+    svc = service({'apikey' => 'a'}, payload)
+    svc.receive_push
+  end
+
+  def service(*args)
+    super Service::Prowl, *args
+  end
+end
+
