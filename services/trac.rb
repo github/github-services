b6a845e032d597846c6f2b526c3b5b4332d9c28d
rick <technoweenie@gmail.com>
2011-06-09 10:49:48 -0700

    port trac hook

--- services/trac.rb
+++ services/trac.rb
@@ -1,19 +1,9 @@
-service :trac do |data, payload|
-  begin
-    #The data['url'] may contain a subdirectory, so this fails
-    #url = URI.join(data['url'], '/github/', data['token'])
-    url_value = data['url'].chomp('/')
-    url = URI.parse("#{url_value}/github/#{data['token']}")
-    req = Net::HTTP::Post.new(url.path)
-    req.set_form_data({"payload" => payload.to_json})
-    if url.user
-      req.basic_auth url.user, url.password
-    end
-    http_session = Net::HTTP.new(url.host, url.port)
-    http_session.use_ssl = true if url.port == 443
-    http_session.start {|http| http.request(req)}
-  rescue Net::HTTPBadResponse => boom
-    raise GitHub::ServiceConfigurationError, "Invalid configuration"
+class Service::Trac < Service
+  self.hook_name = :trac
+
+  def receive_push
+    http.url_prefix = data['url']
+    http_post "github/#{data['token']}", :payload => payload.to_json
   rescue Errno::ECONNREFUSED => boom
     raise GitHub::ServiceConfigurationError, "Connection refused. Invalid server URL."
   end
