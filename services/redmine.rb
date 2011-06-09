c91708a407abd18afeaf72b22b917905ab8b3dfd
rick <technoweenie@gmail.com>
2011-06-09 12:44:36 -0700

    port redmine hook

--- services/redmine.rb
+++ services/redmine.rb
@@ -1,15 +1,12 @@
-service :redmine do |data, payload|
-    url = URI.join(data['address'], "sys/fetch_changesets?key=#{URI.escape(data['api_key'])}&id=#{URI.escape(data['project'])}")
-    if (url.scheme == "http")
-        Net::HTTP.get(url)
-    elsif (url.scheme == "https")
-        # HTTPS
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        request = Net::HTTP::Get.new(url.request_uri)
-        http.start{|http| http.request(request)}
-    else
-        # do nothing
-    end 
+class Service::Redmine < Service
+  self.hook_name = :redmine
+
+  def receive_push
+    http.url_prefix = data['address']
+    http_get "sys/fetch_changesets" do |req|
+      req.params['key'] = data['api_key']
+      req.params['id']  = data['project']
+    end
+  end
 end
 
