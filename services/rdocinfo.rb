5db38a573cea4a406ba86d12501e3cf71a15ae42
rick <technoweenie@gmail.com>
2011-06-09 10:06:21 -0700

    port rubydocinfo hook

--- services/rdocinfo.rb
+++ services/rdocinfo.rb
@@ -1,4 +1,7 @@
-service :rubydocinfo do |data, payload|
-  Net::HTTP.post_form(URI.parse("http://rubydoc.info/checkout"), :payload => JSON.generate(payload))
-  "namaste"
+class Service::RubyDocInfo < Service
+  self.hook_name = :rubydocinfo
+
+  def receive_push
+    http_post 'http://rubydoc.info/checkout', :payload => payload.to_json
+  end
 end
