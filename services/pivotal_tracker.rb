daac633ee8576e150239e4c926703d08a8462f34
risk <technoweenie@gmail.com>
2011-06-12 14:43:13 -0700

    get pivotal tracker hook working

--- services/pivotal_tracker.rb
+++ services/pivotal_tracker.rb
@@ -1,6 +1,10 @@
 class Service::PivotalTracker < Service
   def receive_push
     token = data['token']
+
+    # need to figure out how to get the right equifax secure ca loaded
+    http.ssl[:verify] = false
+
     res = http_post 'https://www.pivotaltracker.com/services/v3/github_commits' do |req|
       req.params[:token] = data['token']
       req.body = {:payload => payload.to_json}
