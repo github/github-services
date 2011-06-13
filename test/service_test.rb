2405f275d0864b16fe66bdcb2f84d2648b8aab6c
risk <technoweenie@gmail.com>
2011-06-12 18:15:59 -0700

    add test for Service.receive

--- test/service_test.rb
+++ test/service_test.rb
@@ -2,11 +2,21 @@ require File.expand_path('../helper', __FILE__)
 
 class ServiceTest < Service::TestCase
   class TestService < Service
+    def receive_push
+    end
   end
 
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
-    @service = service('data', 'payload')
+    @service = service(:push, 'data', 'payload')
+  end
+
+  def test_receive_valid_event
+    assert TestService.receive :push, {}, {}
+
+    assert_raise ArgumentError do
+      TestService.receive :star, {}, {}
+    end
   end
 
   def test_url_shorten
