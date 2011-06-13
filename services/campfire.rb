b3e634ead59f92145e0f5907487651d942ab8024
risk <technoweenie@gmail.com>
2011-06-12 18:09:46 -0700

    rescue the NoMethodError from inside the tinder gem if there are no rooms

--- services/campfire.rb
+++ services/campfire.rb
@@ -11,28 +11,31 @@ class Service::Campfire < Service
       messages.first << " (#{distinct_commits.first['url']})"
     end
 
-    begin
-      play_sound = data['play_sound'].to_i == 1
+    play_sound = data['play_sound'].to_i == 1
 
-      if !campfire.login(data['token'], 'X')
-        raise_config_error 'Invalid campfire token'
-      end
-
-      unless room = campfire.find_room_by_name(data['room'])
-        raise_config_error 'No such campfire room'
-      end
-
-      messages.each { |line| room.speak line }
-      room.play "rimshot" if play_sound && room.respond_to?(:play)
-
-      campfire.logout
-    rescue Errno::ECONNREFUSED => boom
-      raise_config_error "Connection refused- invalid campfire subdomain."
+    if !campfire.login(data['token'], 'X')
+      raise_config_error 'Invalid campfire token'
     end
+
+    unless room = find_room
+      raise_config_error 'No such campfire room'
+    end
+
+    messages.each { |line| room.speak line }
+    room.play "rimshot" if play_sound && room.respond_to?(:play)
+
+    campfire.logout
+  rescue Faraday::Error::ConnectionFailed
+    raise_config_error "Connection refused- invalid campfire subdomain."
   end
 
   attr_writer :campfire
   def campfire
     @campfire ||= Tinder::Campfire.new(data['subdomain'], :ssl => true)
+  end
+
+  def find_room
+    room = campfire.find_room_by_name(data['room'])
+  rescue StandardError
   end
 end
