1831b7c7a4f04a4be70eec88afc6b7a64ede8f79
risk <technoweenie@gmail.com>
2011-06-15 12:35:41 -0400

    cant verify trac ssl

--- services/trac.rb
+++ services/trac.rb
@@ -1,5 +1,6 @@
 class Service::Trac < Service
   def receive_push
+    http.ssl[:verify] = false
     http.url_prefix = data['url']
     http_post "github/#{data['token']}", :payload => payload.to_json
   rescue Faraday::Error::ConnectionFailed
