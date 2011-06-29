985686a77ff44f3f94221cf94dddf8daf14c5b68
risk <technoweenie@gmail.com>
2011-06-29 08:08:37 -0600

    i could let this injustice go on no longer

--- services/zohoprojects.rb
+++ services/zohoprojects.rb
@@ -1,12 +1,12 @@
 class Service::ZohoProjects < Service
   def receive_push
-	res=http_post "https://projects.zoho.com/serviceHook",
-	:pId => data['project_id'],
-	:authtoken => data['token'],
-	:scope => "projectsapi",
-	:payload => JSON.generate(payload)
-	if res.status != 200
-      	raise_config_error
- 	end
+    res = http_post "https://projects.zoho.com/serviceHook",
+      :pId       => data['project_id'],
+      :authtoken => data['token'],
+      :scope     => "projectsapi",
+      :payload   => JSON.generate(payload)
+    if res.status != 200
+      raise_config_error
+    end
   end
 end
--- test/zohoprojects_test.rb
+++ test/zohoprojects_test.rb
@@ -1,26 +1,25 @@
 require File.expand_path('../helper', __FILE__)
 
 class ZohoProjectsTest < Service::TestCase
-  
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
-  
+
   def data
     {
       "project_id" => "1234",
       "token" => "a13d",
     }
   end
-  
+
   def test_push
     url = "/serviceHook"
     @stubs.post url do |env|
 	    assert_match /(^|\&)pId=1234($|\&)/, env[:body]
 	    assert_match /(^|\&)authtoken=a13d($|\&)/, env[:body]
       [200, {}, '']
-    end 
-	  
+    end
+	
     svc = service :push, data , payload
     svc.receive
   end
