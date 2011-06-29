4a5b6200586f59cd819a038eacc52366533b1746
risk <technoweenie@gmail.com>
2011-06-29 08:31:56 -0600

    add service titles

--- services/masterbranch.rb
+++ services/masterbranch.rb
@@ -1,4 +1,4 @@
-class Service::MasterBranch < Service
+class Service::Masterbranch < Service
   def receive_push
     http_post "http://webhooks.masterbranch.com/gh-hook",
       :payload => payload.to_json
