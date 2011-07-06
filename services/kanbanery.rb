07497279f91812c1c9c3c0ee8af712f2f20fbaa6
Hanna Seweryn <hanna.seweryn@llp.pl>
2011-07-06 10:55:26 +0200

    update kanbanery hook to use new services API

--- services/kanbanery.rb
+++ services/kanbanery.rb
@@ -1,15 +1,17 @@
-site = "http://kanbanery.com"
-@uri = URI.parse(site)
+class Service::Kanbanery < Service
 
-service :kanbanery do |data, payload|
-  project_id = data['project_id']
-  token = data['project_token']
+  def receive_push
+    uri = URI.parse("http://kanbanery.com")
+    project_id = data['project_id']
+    token = data['project_token']
 
-  @uri.path = "/api/v1/projects/#{project_id}/git_commits"
-  http = Net::HTTP.new(@uri.host, @uri.port)
-  request = Net::HTTP::Post.new(@uri.request_uri, {'X-Kanbanery-ProjectGitHubToken' => token})
-  request.body = payload.to_json
-  request.content_type = 'application/json'
-  response = http.request(request)
+    uri.path = "/api/v1/projects/#{project_id}/git_commits"
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, {'X-Kanbanery-ProjectGitHubToken' => token})
+    request.body = payload.to_json
+    request.content_type = 'application/json'
+    response = http.request(request)
+
+  end
   
 end
