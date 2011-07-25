6591be31c370f757734dd54878216fbe2a45902b
Martin Drapeau <martin@planbox.com>
2011-07-25 09:21:27 -0400

    Added Planbox service.

---
+++ services/planbox.rb
@@ -0,0 +1,19 @@
+class Service::Planbox < Service
+  string :token
+
+  def receive_push
+    token = data['token']
+	
+    http.ssl[:verify] = false
+
+    res = http_post 'https://www.planbox.com/api/github_commits' do |req|
+      req.params[:token] = data['token']
+      req.body = {:payload => payload.to_json}
+    end
+
+    if res.status < 200 || res.status > 299
+      raise_config_error
+    end
+  end
+end
+
