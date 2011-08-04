957b642939a9861dd0160b3cbd60a45f449cd4d0
Corey Donohoe <atmos@atmos.org>
2011-08-04 10:05:57 -0700

    use the bit.ly key from the secrets file

--- lib/service.rb
+++ lib/service.rb
@@ -251,7 +251,7 @@ class Service
         :version => '2.0.1',
         :longUrl => url,
         :login   => 'github',
-        :apiKey  => 'R_261d14760f4938f0cda9bea984b212e4'
+        :apiKey  => self.class.secrets['bitly']['key'] || 'R_261d14760f4938f0cda9bea984b212e4'
     end
 
     short = JSON.parse(res.body)
