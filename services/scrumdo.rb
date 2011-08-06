0edf239c435c962b492300519e4ed981262d5879
Marc Hughes <mhughes@tomsnyder.com>
2011-08-06 15:49:49 -0400

    Some cleanup

--- services/scrumdo.rb
+++ services/scrumdo.rb
@@ -6,9 +6,7 @@ class Service::ScrumDo < Service
   def receive_push
     username = data["username"]
     password = data["password"]
-    project_slug = data["project_slug"]
 
-    # url = "http://localhost:8000/hooks/github/%s" % [project_slug]
     url = "http://www.scrumdo.com/hooks/github/#{data["project_slug"]}"
     res = http_post url do |req|
        req.body = {:payload => payload.to_json, :username=>username, :password=>password}
