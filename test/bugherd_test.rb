397c867a673f1e3dbd4c75246473d190c2a5fb5f
root <root@crucial.brendel>
2011-08-11 19:56:48 +1000

    add tests

--- test/bugherd_test.rb
+++ test/bugherd_test.rb
@@ -4,6 +4,17 @@ class BugherdTest < Service::TestCase
   end
 
   def test_push
+    @stubs.post "/custom" do |env|
+      assert_equal 'www.example.com', env[:url].host
+      assert_equal 'application/x-www-form-urlencoded',
+        env[:request_headers]['content-type']
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
+    svc.receive_push
+
     @stubs.post "/github_web_hook/KEY" do |env|
       assert_equal 'www.bugherd.com', env[:url].host
       assert_equal 'application/x-www-form-urlencoded',
