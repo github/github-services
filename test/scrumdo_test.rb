852cc33c9dd32614b17ad2d150df3aad500b2282
risk :danger: olson <technoweenie@gmail.com>
2011-08-18 14:46:17 -0700

    scrumdo test

---
+++ test/scrumdo_test.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../helper', __FILE__)
+
+class ScrumDoTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    @stubs.post "/hooks/github/slug" do |env|
+      assert_equal 'www.scrumdo.com', env[:url].host
+      data = Rack::Utils.parse_query(env[:body])
+      assert_equal 'rick',   data['username']
+      assert_equal 'monkey', data['password']
+      assert data['payload']
+      [200, {}, '{}']
+    end
+
+    svc = service({
+      'username' => 'rick',
+      'password' => 'monkey',
+      'project_slug' => 'slug'
+    }, payload)
+
+    svc.receive_push
+  end
+
+  def service(*args)
+    super Service::ScrumDo, *args
+  end
+end
+
+
+
+
