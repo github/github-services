7555a5403293fa5ccccdb2906493af38482b62e7
John Haley <johnhaley81@gmail.com>
2011-09-13 09:51:12 -0700

    Fixed a syntax error

--- services/ontime.rb
+++ services/ontime.rb
@@ -4,7 +4,7 @@ class Service::OnTime < Service
 	def receive_push
 		if data['ontime_url'].to_s.empty?
 			raise_config_error "No OnTime URL to connect to."
-		elsif data['api_key'].to_s.empty
+		elsif data['api_key'].to_s.empty?
 			raise_config_error "No API Key."
 		end
 		
