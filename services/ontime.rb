802c5c64ed3c6a633c56d5d1a8e3c1edec6b12f6
John Haley <johnhaley81@gmail.com>
2011-09-16 08:27:55 -0700

    Instead of doing a post for each commit that is related to OnTime, we are just going to send back the entire payload each time a commit is made and process it in OnTime.

--- services/ontime.rb
+++ services/ontime.rb
@@ -8,55 +8,18 @@ class Service::OnTime < Service
 			raise_config_error "No API Key."
 		end
 		
-		owner 		= payload['repository']['owner']['name']
-		repo_name 	= payload['repository']['name']
+		#We're just going to send back the entire payload and process it in OnTime.
+		http.headers['Content-Type'] = 'application/json'
+		http.url_prefix = data['ontime_url']
 		
-		payload['commits'].each do |commit|
-			files 			= commit['added'] | commit['modified'] | commit['removed']
-			ontime_items	= Array.new
-			
-			# Look to see if there is a OnTime item id/type in the commit message
-			# [OnTime.<type>.<id>] [OnTime.defect.5] [ontime.d.5] [ontime.t.123] [ontime.task.123]
-			# This is a commit comment [ontime.defect.5] and it belongs also to [ontime.task.123]
-			ontime_item_ids = commit['message'].scan(/\[[\s]*ontime\.(defect|feature|task)\.([1-9][0-9]*)[\s]*\]/i)
-			
-			ontime_item_ids.each do |ot_item|
-				item = {}
-				item['type'] 	= ot_item.at(0)
-				item['id']		= ot_item.at(1)
-				ontime_items << item
-			end
-			
-			if ontime_items.size != 0
-				http.headers['Content-Type'] = 'application/json'
-				http.url_prefix = data['ontime_url']
-				
-				postdata = {}
-				postdata['commit_msg']		= commit['message']
-				postdata['commit_url']		= commit['url']
-				postdata['commit_id']		= commit['id']
-				postdata['commit_time']		= commit['timestamp']
-				postdata['commit_author']	= commit['author']['name']
-				postdata['ontime_items']	= ontime_items
-				postdata['files'] 			= Array.new
-				
-				files.each do |file|
-					file_with_url = {}
-					file_with_url['filename'] = file
-					file_with_url['url'] = "https://github.com/" + owner + "/" + repo_name + "/blob/" + commit['id'] + "/" + file
-					
-					postdata['files'] << file_with_url
-				end
-				
-				sha256 = Digest::SHA2.new(256)
-				hash = sha256.digest(postdata.to_json + api_key)
-				
-				result = http_post "api/scm_files", :postdata => postdata.to_json, :hash => hash, :source => :github
-				
-				if(result.status != 200)
-					raise_config_error "Post status returned: " + result.status
-				end
-			end
+		#Hash the data
+		sha256 = Digest::SHA2.new(256)
+		hash = sha256.digest(payload.to_json + api_key)
+		
+		result = http_post "api/scm_files", :payload => payload.to_json, :hash => hash, :source => :github
+		
+		if(result.status != 200)
+			raise_config_error "Post status returned: " + result.status
 		end
 	end
 end
