a5c7cfe075f2f0a562fe2f69a84354ec480a8141
Rune Sørensen <runesoerensen@gmail.com>
2011-09-19 04:35:51 -0700

    Make sure we've got a build url

--- services/appharbor.rb
+++ services/appharbor.rb
@@ -2,5 +2,6 @@ class Service::AppHarbor < Service
   string :create_build_url
   
   def receive_push
+    raise_config_error 'Missing Create build URL' if data['create_build_url'].to_s.empty?
   end
 end
