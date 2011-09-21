dc0ed2b03ac885bd57a0a80878065210c38eb0a5
Rune Sørensen <runesoerensen@gmail.com>
2011-09-20 19:43:43 -0700

    Add appharbor formatted message object

--- services/appharbor.rb
+++ services/appharbor.rb
@@ -5,5 +5,14 @@ class Service::AppHarbor < Service
     raise_config_error 'Missing Create build URL' if data['create_build_url'].to_s.empty?
 
     commit = distinct_commits.last
+    appharbor_message = {
+      :branches => {
+        ref_name => {
+          :commit_id => commit["id"],
+          :commit_message => commit["message"],
+          :download_url => commit["url"].sub("commit", "tarball")
+        }
+      }
+    }
   end
 end
