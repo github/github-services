ea984fe1e3de6b9db20f9f2eae0fedd0f49cf95a
Rune Sørensen <runesoerensen@gmail.com>
2011-09-20 19:57:15 -0700

    Extract create build url to variable

--- services/appharbor.rb
+++ services/appharbor.rb
@@ -2,7 +2,9 @@ class Service::AppHarbor < Service
   string :create_build_url
   
   def receive_push
-    raise_config_error 'Missing Create build URL' if data['create_build_url'].to_s.empty?
+    create_build_url = data['create_build_url']
+
+    raise_config_error 'Missing Create build URL' if create_build_url.to_s.empty?
 
     commit = distinct_commits.last
     appharbor_message = {
@@ -15,6 +17,6 @@ class Service::AppHarbor < Service
       }
     }
 
-    http_post data["create_build_url"], appharbor_message.to_json, 'Accept' => 'application/json'
+    http_post create_build_url, appharbor_message.to_json, 'Accept' => 'application/json'
   end
 end
