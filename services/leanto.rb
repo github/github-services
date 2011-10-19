7737c8b99e377aa63a1872a6267d4ebdbb7bb5c8
risk :danger: olson <technoweenie@gmail.com>
2011-10-19 13:04:17 -0700

    Leanto => Lean-To

--- services/leanto.rb
+++ services/leanto.rb
@@ -1,6 +1,8 @@
 class Service::Leanto < Service
   string :token
 
+  self.title = 'Lean-To'
+
   def receive_push
     res = http_post "https://www.lean-to.com/api/%s/commit" %
       [ data['token'] ],
