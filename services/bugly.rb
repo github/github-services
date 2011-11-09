69e3741b97457a64adeb5b213b02f4d304499c80
rick <technoweenie@gmail.com>
2011-11-09 14:38:55 -0700

    Update services/bugly.rb

--- services/bugly.rb
+++ services/bugly.rb
@@ -2,6 +2,7 @@ class Service::Bugly < Service
   string :project_id, :account_name, :token
 
   def receive_push
+    http.ssl[:verify] = false # :(
     http_post "https://#{data['account_name']}.bug.ly/changesets.json?service=github&project_id=#{data['project_id']}",
       JSON.generate(payload),
       'X-BuglyToken' => data['token'],
