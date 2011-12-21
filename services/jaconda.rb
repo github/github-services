bc56216c90bda0adffd4cd78349d3e8ac87a80ca
Anton Mironov <ant.mironov@gmail.com>
2011-12-21 21:17:54 +0600

    Send all events to Jaconda

--- services/jaconda.rb
+++ services/jaconda.rb
@@ -2,15 +2,20 @@ class Service::Jaconda < Service
   string  :subdomain, :room_id, :room_token
   boolean :digest
 
-  def receive_push
-    raise_config_error "Missing 'subdomain'"  if data['subdomain'].to_s == ''
-    raise_config_error "Missing 'room_id'"    if data['room_id'].to_s == ''
+  default_events :commit_comment, :download, :fork, :fork_apply, :gollum,
+    :issues, :issue_comment, :member, :public, :pull_request, :push, :watch
+
+  def receive_event
+    raise_config_error "Missing 'subdomain'" if data['subdomain'].to_s.empty?
+    raise_config_error "Missing 'room_id'"   if data['room_id'].to_s.empty?
 
     http.basic_auth data['room_token'], 'X'
 
     res = http_post "https://#{data['subdomain']}.jaconda.im/api/v2/rooms/#{data['room_id']}/notify/github.json",
       :payload => JSON.generate(payload),
-      :digest => data['digest']
+      :digest => data['digest'],
+      :branch_name => branch_name,
+      :event => event
 
     if res.status < 200 || res.status > 299
       raise_config_error "#{res.status}: #{res.body}"
