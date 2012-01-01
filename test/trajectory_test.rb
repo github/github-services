de0445ee91f8ddf75f7a7ad92edd6b4e640ad558
Avi Tzurel <avi@kensodev.com>
2012-01-01 19:13:45 +0200

    Trajectory and test

---
+++ test/trajectory_test.rb
@@ -0,0 +1,21 @@
+require File.expand_path('../helper', __FILE__)
+
+class TrajectoryTest < Service::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_push
+    @stubs.post '/api/payloads?api_key=test_api_key' do |env|
+      assert_equal 'application/json', env[:request_headers]['Content-Type']
+      [200, {}, '']
+    end
+
+    svc = service({'api_key' => 'test_api_key'})
+    svc.receive_push
+  end
+
+  def service(*args)
+    super Service::Trajectory, *args
+  end
+end
