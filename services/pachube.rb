3ba4eb3bb759f1680e6c4d1a1e0a1730a317c201
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-03-09 13:21:57 +0000

    Extended Pachube hook with extra stats

--- services/pachube.rb
+++ services/pachube.rb
@@ -12,10 +12,32 @@ class Service::Pachube < Service
         :version => '1.0.0',
         :datastreams => [
           {
-            :id => repo_name,
+            :id => "#{repo_name}.commits_pushed",
             :current_value => distinct_commits.size
           }
         ]}.to_json
+    end
+
+    payload['commits'].each do |commit|
+      http_method :put, "https://api.pachube.com/v2/feeds/#{data['feed_id']}.json" do |req|
+        req.headers['X-PachubeApiKey'] = data['api_key']
+        req.body = {
+          :version => '1.0.0',
+          :datastreams => [
+            {
+              :id => "#{repo_name}.modified_files",
+              :current_value => commit['modified']
+            },
+            {
+              :id => "#{repo_name}.added_files",
+              :current_value => commit['added']
+            },
+            {
+              :id => "#{repo_name}.removed_files",
+              :current_value => commit['removed']
+            }
+          ]}.to_json
+      end
     end
   end
 end
