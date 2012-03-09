a5dfe71fa2e6d40e393baf1af0398d0843ecc8d1
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-03-09 16:30:17 +0000

    Implemented most of the basic stuff for Pachube hook

--- services/pachube.rb
+++ services/pachube.rb
@@ -6,33 +6,34 @@ class Service::Pachube < Service
   def receive_push
     raise_config_error "Missing api_key" if data['api_key'].to_s.empty?
     raise_config_error "Missing feed_id" if data['feed_id'].to_s.empty?
+    raise_config_error "Missing track_branch" if data['track_branch'].to_s.empty?
 
     feed_url = "https://api.pachube.com/v2/feeds/#{data['feed_id']}"
 
-    if payload['ref'] == "refs/head/#{data[:track_branch]}" then
-      http_method :put, "#{feed_url}/#{data['feed_id']}.json" do |req|
+    if payload['ref'] == "refs/heads/#{data['track_branch']}" then
+      http_method :put, "#{feed_url}.json" do |req|
         req.headers['X-PachubeApiKey'] = data['api_key']
         req.body = {
           :version => '1.0.0',
           :datastreams => [
             {
-              :id => "#{repo_name}.commits_pushed",
+              :id => "#{repo_name}-commits_pushed",
               :current_value => distinct_commits.size
             },
             {
-              :id => "#{repo_name}.modified"
+              :id => "#{repo_name}-files_modified"
             },
             {
-              :id => "#{repo_name}.removed"
+              :id => "#{repo_name}-files_removed"
             },
             {
-              :id => "#{repo_name}.added"
+              :id => "#{repo_name}-files_added"
             }
           ]}.to_json
       end
       distinct_commits.each do |commit|
         [ 'modified', 'removed', 'added' ].each do |ds|
-          http_method :post, "#{feed_url}/datastreams/#{repo_name}.#{ds}/datapoints.json" do |req|
+          http_method :post, "#{feed_url}/datastreams/#{repo_name}-files_#{ds}/datapoints" do |req|
             req.headers['X-PachubeApiKey'] = data['api_key']
             req.body = {
               :version => '1.0.0',
