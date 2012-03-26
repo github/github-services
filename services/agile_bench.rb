b78e6350e4954838c4c1143a6909420be3ddabb5
Mark Mansour <mark@stateofflux.com>
2012-03-26 14:19:49 +1100

    Replaced test URL with production URL and added more tests

--- services/agile_bench.rb
+++ services/agile_bench.rb
@@ -2,19 +2,45 @@ class Service::AgileBench < Service
   string :token, :project_id
 
   def receive_push
-    token, project_id =
-     data['token'].to_s.strip, data['project_id'].to_s.strip
+    ensure_required_data
+    post_to_agile_bench
+    verify_response
+  end
 
-    raise_config_error "Invalid Token" if !token.present?
-    raise_config_error "Invalid Project ID" if !project_id.present?
+  protected
+  def ensure_required_data
+    raise_config_error "Token is required"      if !token.present?
+    raise_config_error "Project ID is required" if !project_id.present?
+  end
 
-    response = { :token       => token,
-                 :payload     => payload }
+  def post_to_agile_bench
+    @response = http_post "http://agilebench.com/services/v1/github" do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = JSON.generate({
+        :payload => payload,
+        :data => {
+          :project_id => project_id,
+          :token => token
+        }
+      })
+    end
+  end
 
-    puts "response: " + response.to_json
+  def token
+    @token ||= data['token'].to_s.strip
+  end
 
-    res = http_post "http://212.127.65.121:9393/project/#{project_id}",
-      { :data => response.to_json }
+  def project_id
+    @project_id ||= data['project_id'].to_s.strip
+  end
+
+  def verify_response
+    case @response.status
+      when 200..299
+      when 403, 401, 422 then raise_config_error("Invalid Credentials")
+      when 404, 301, 302 then raise_config_error("Invalid URL")
+      else raise_config_error("HTTP: #{@response.status}")
+    end
   end
 end
 
--- test/agile_bench_test.rb
+++ test/agile_bench_test.rb
@@ -3,19 +3,37 @@ require File.expand_path('../helper', __FILE__)
 class AgileBenchTest < Service::TestCase
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
-    @token = "test_token"
-    @project_id = "123"
   end
 
   def test_push
-    @stubs.post "/project/#{@project_id}" do |env|
-      assert_equal '212.127.65.121', env[:url].host
+    @stubs.post "/services/v1/github" do |env|
+      assert_equal 'agilebench.com', env[:url].host
+      assert_equal 'application/json',
+        env[:request_headers]['Content-type']
       [200, {}, '']
     end
 
-    svc = service(
-      {'token' => 'test_token', 'project_id' => '123'}, payload)
+    svc = service({'token' => 'test_token', 'project_id' => '123'},
+                  payload)
     svc.receive_push
+  end
+
+  def test_missing_token
+    @stubs.post "/services/v1/github"
+    svc = service({'token' => '', 'project_id' => '123'},
+                  payload)
+    assert_raise Service::ConfigurationError do
+      svc.receive_push
+    end
+  end
+
+  def test_missing_project_id
+    @stubs.post "/services/v1/github"
+    svc = service({'token' => 'test_token', 'project_id' => ''},
+                  payload)
+    assert_raise Service::ConfigurationError do
+      svc.receive_push
+    end
   end
 
   def service(*args)
