017473fca19c444d3e250bd9bb3a8702b9426a22
Nanda Kishore <jnandakishore@zeomega.com>
2012-04-10 18:53:09 +0530

    Functionality related to "Notifying, when a repo visibility is switched from Private to Public" is in place. So are the testcases for the total service code. Testcases related to

--- services/pubalert.rb
+++ services/pubalert.rb
@@ -7,8 +7,10 @@ class Service::PubAlert < Service
     # (Required) The remote URL to POST data towards (eg: "https://com.example/webhook/github"
     :auth_token,
     # (Required) Authentication Token to be used by the POST URL
-    :repo_name
+    :repo_name,
     # (Required) Repo name which triggered this Event.
+    :notify_email
+    # (Optional) Email Address(1) to intimate that Public Event is generated
 
   default_events :public
 
@@ -18,15 +20,40 @@ class Service::PubAlert < Service
     raise_config_error "Missing 'auth_token'" if data['auth_token'].to_s == ''
     raise_config_error "Missing 'repo_name'" if data['repo_name'].to_s == ''
 
+    notify_event data['notify_email'], data['repo_name'] if data['repo_name'].to_s.size > 0
+
     # Set our headers
     http.headers['X-GitHub-Event'] = event.to_s #this should be 'public' anyway
 
-    res = http_post "#{data['remote_url']}",{
-      :auth_token => data['auth_token'],
-      :repo_name => data['repo_name']
-        }
+    res = http_post "#{data['remote_url']}",
+            {:auth_token => data['auth_token'],
+             :repo_name => data['repo_name']}
     if res.status < 200 || res.status > 299
       raise_config_error
     end
+  end
+
+  def notify_event(address, repo_name)
+    my = self
+    Mail.defaults do
+      delivery_method :smtp, { :address   => my.email_config['address'],
+                           :port      => my.email_config['port'],
+                           :domain    => my.email_config['domain'],
+                           :user_name => my.email_config['user_name'],
+                           :password  => my.email_config['password'],
+                           :authentication => my.email_config['authentication'],
+                           :enable_starttls_auto => my.email_config['enable_starttls_auto']}
+    end
+    mail_message = Mail.deliver do
+      to       address
+      from     "GitHub <noreply@github.com>"
+      reply_to "GitHub <noreply@github.com>"
+      subject  "#{repo_name} is open-sourced."
+      text_part do
+        content_type 'text/plain; charset=UTF-8'
+        body         "#{repo_name} is open-sourced."
+      end
+    end
+    mail_message
   end
 end
--- test/pubalert_test.rb
+++ test/pubalert_test.rb
@@ -6,19 +6,47 @@ class PubAlertTest < Service::TestCase
   end
 
   def test_public
-    @stubs.post "http://127.0.0.1:8000/make_repo_private" do |env|
-      env.inspect
+    data = {'remote_url'   => 'http://127.0.0.1:8000/make_repo_private/',
+            'auth_token'   => 'sometoken',
+            'repo_name'    => 'testrepo',
+            'notify_email' => 'xyz@domain.com'}
+
+    svc = service :public, data
+
+    @stubs.post "/make_repo_private/" do |env|
+      assert_equal 'public', env[:request_headers]['x-github-event']
+      assert_match /form/, env[:request_headers]['content-type']
+      assert_equal '127.0.0.1', env[:url].host
+      assert_nil env[:request_headers]['X-Hub-Signature']
       [200, {}, '']
     end
 
-    svc = service :public,
-      {'remote_url' => 'http://127.0.0.1:8000/make_repo_private',
-       'auth_token' => 'sometoken',
-       'repo_name'  => 'testrepo'}
     svc.receive_public
+
+    assert svc.messages
+    assert_equal svc.messages.size, 1
+    msg = svc.messages.shift
+    assert msg
+    assert_equal msg['to'], data['notify_email']
+    assert_equal msg['from'], "GitHub <noreply@github.com>"
+    assert_equal msg['reply_to'], "GitHub <noreply@github.com>"
+    assert_equal msg['subject'], "#{data['repo_name']} is open-sourced."
+    assert_equal msg['body'], "#{data['repo_name']} is open-sourced."
   end
 
   def service(*args)
-    super Service::PubAlert, *args
+    svc = super Service::PubAlert, *args
+    def svc.messages
+      @messages ||= []
+    end
+
+    def svc.notify_event(address, repo_name)
+       messages << {'to' => address,
+                     'from' => "GitHub <noreply@github.com>",
+                     'reply_to' => "GitHub <noreply@github.com>",
+                     'subject' => "#{repo_name} is open-sourced.",
+                     'body' => "#{repo_name} is open-sourced."}
+    end
+    svc
   end
 end
