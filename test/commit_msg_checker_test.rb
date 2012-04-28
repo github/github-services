d7c7595b852c325663229111b44009475775b589
marko asplund <marko.asplund@ixonos.com>
2012-04-28 22:26:41 +0300

    finalize unit test

--- test/commit_msg_checker_test.rb
+++ test/commit_msg_checker_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('../helper', __FILE__)
 class Service::CommitMsgChecker < Service
   def configure_delivery(config)
     Mail.defaults do
-      delivery_method :file
+      delivery_method :test
     end
     @@mail_configured = true
   end
@@ -24,14 +24,28 @@ class CommitMsgCheckerTest < Service::TestCase
   end
 
   def test_push
-    payload_file = File.new("github-event.js")
-    hash = eval(payload_file.read)
-
-    svc = service(hash['data'], hash['payload'])
+#    payload_file = File.new("github-event.js")
+    payload_file = File.new("docs/github_payload")
+    payload = eval(payload_file.read)
+    
+    svc = service({
+      'message_format' => '\[#WEB-\d{1,5} status:\d+ resolution:\d+\] .*$',
+      'recipients' => "a@b.fi, c@d.fi"
+    }, payload['payload'])
     svc.configure_delivery([])
 
     svc.receive_push
-
+    
+    assert_equal 1, Mail::TestMailer.deliveries.length
+    m = Mail.new(Mail::TestMailer.deliveries.shift)    
+    assert_equal "[mojombo/grit] commit message format is invalid", m.subject
+    assert_equal ["tom@mojombo.com"], m.to
+    assert_equal ["a@b.fi", "c@d.fi"], m.cc
+    assert_match "commit/5057e76a11abd02e83b7d3d3171c4b68d9c88480", m.body.to_s
+    assert_match "commit/a47fd41f3aa4610ea527dcc1669dfdb9c15c5425", m.body.to_s
+    assert_no_match %r{commit/06f63b43050935962f84fe54473a7c5de7977325}, m.body.to_s
+    
+    #puts m
   end
   
   def service(*args)
