36e77f59421a7ab77da2fcd45d3c6a4f988585b9
Frank LoVecchio <franklovecchio@gmail.com>
2012-05-03 10:58:26 -0600

    Add username/password support.

--- services/mqtt.rb
+++ services/mqtt.rb
@@ -1,5 +1,5 @@
 class Service::MqttPub < Service
-  string :broker, :port, :topic, :id
+  string :broker, :port, :topic, :id, :username, :password
   
   require 'mqtt'
 
@@ -9,6 +9,7 @@ class Service::MqttPub < Service
       raise_config_error "Invalid broker endpoint."
     end
     
+    # Optional - use standard MQTT port if not specified
     port = data['port'].to_i
     if data['port'].to_s == '' 
       port = 1883
@@ -18,18 +19,32 @@ class Service::MqttPub < Service
       raise_config_error "Invalid topic. Try github/<github_username>/<repo_name> ."
     end
     
-    # Generate random epoch for ID if not specified
+    # Optional - generate random epoch for ID if not specified
     client_id = data['id'].to_s
     if client_id == ''
       client_id = Time.now.to_i.to_s
     end
-          
+    
+    # Optional, specify nil if not specified (per http://rubydoc.info/gems/mqtt/MQTT/Client)
+    username = data['username'].to_s
+    if username == '' and passwor 
+      username = nil
+    end
+    
+    # Optional, specify nil if not specified
+    password = data['password'].to_s
+    if password == '' 
+      password = nil
+    end
+
     begin
       # Connect to the broker, publish the payload!    
       MQTT::Client.connect(
         :remote_host => data['broker'].to_s, 
         :remote_port => port,
         :client_id => client_id
+        :username => username,
+        :password => password,
       ) do |client|
           client.publish(data['topic'].to_s, payload.to_json)
           # Disconnect (don't send last will and testament)
