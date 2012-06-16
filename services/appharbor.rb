8c2a4e88584dbb8e3a202129f235fb538060af28
Michael Friis <friism@gmail.com>
2012-06-16 18:41:52 +0200

    update service

--- services/appharbor.rb
+++ services/appharbor.rb
@@ -8,7 +8,7 @@ class Service::AppHarbor < Service
     raise_config_error 'Missing application slug' if slug.to_s.empty?
     raise_config_error 'Missing token' if token.to_s.empty?
 
-    create_build_url = "https://appharbor.com/applications/#{slug}/builds?authorization=#{token}"
+    create_build_url = "https://appharbor.com/applications/#{slug}/builds"
 
     commit = distinct_commits.last
     appharbor_message = {
@@ -21,6 +21,9 @@ class Service::AppHarbor < Service
       }
     }
 
-    http_post create_build_url, appharbor_message.to_json, 'Accept' => 'application/json'
+    http.headers['Accept'] = 'application/json'
+    http.headers['Authorization'] = "BEARER #{token}"
+
+    http_post create_build_url, appharbor_message.to_json
   end
 end
