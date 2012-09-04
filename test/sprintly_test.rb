67675225e8a96b73e3d1de8aba3b7a8986a5fa18
Justin Lilly <justin@justinlilly.com>
2012-09-04 11:22:32 -0700

    Updating tests to actually test the right values.

--- test/sprintly_test.rb
+++ test/sprintly_test.rb
@@ -4,20 +4,26 @@ class SprintlyTest < Service::TestCase
   end
 
   def test_push
-    @stubs.post "/integration/github/1/push" do |env|
-      assert_equal 'my-service.com', env[:url].host
+    @stubs.post "/integration/github/1/push/" do |env|
+      assert_equal 'sprint.ly', env[:url].host
       assert_equal 'application/json',
         env[:request_headers]['content-type']
-      assert_equal basic_auth("user", "my_api_key"),
+      assert_equal basic_auth("my_user@foo.bar", "my_api_key"),
         env[:request_headers]['authorization']
       [200, {}, '']
     end
 
-    svc = service :push,
-      {'email' => 'my_user@foo.bar', 'product_id' => '1', 'api_key' => 'my_api_key'}, payload
+    svc = service :push, {
+       'email' => 'my_user@foo.bar',
+       'product_id' => '1',
+       'api_key' => 'my_api_key'
+    }, payload
+
     svc.receive_event
+    @stubs.verify_stubbed_calls
   end
 
   def service(*args)
     super Service::Sprintly, *args
   end
+end
