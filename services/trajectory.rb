0d40acaabb54b64c8e5e926aeeffea92c21c354b
Jason Draper <jason@drapergeek.com>
2012-10-22 15:47:53 -0400

    Set up pull request information for Trajectory.

--- services/trajectory.rb
+++ services/trajectory.rb
@@ -1,15 +1,47 @@
 class Service::Trajectory < Service
   string :api_key
+  BASE_URL = "https://www.apptrajectory.com/api/payloads?api_key="
 
   def receive_push
-    raise_config_error "Missing 'api_key'" if data['api_key'].to_s == ''
+    send_to_trajectory
+  end
 
+  def receive_pull_request
+    send_to_trajectory
+  end
+
+  private
+
+  def send_to_trajectory
+    set_http_headers
+    response = send_post
+    raise_config_error_for_bad_status(response)
+  end
+
+  def set_http_headers
     http.headers['content-type'] = 'application/json'
+  end
 
-    res = http_post "https://www.apptrajectory.com/api/payloads?api_key=#{data['api_key'].to_s}", JSON.generate(payload)
+  def send_post
+    http_post full_url, json_payload
+  end
 
-    if res.status < 200 || res.status > 299
+  def full_url
+    BASE_URL + api_key
+  end
+
+  def raise_config_error_for_bad_status(response)
+    if response.status < 200 || response.status > 299
       raise_config_error
     end
+  end
+
+  def api_key
+    raise_config_error "Missing 'api_key'" if data['api_key'].to_s == ''
+    data['api_key'].to_s
+  end
+
+  def json_payload
+    JSON.generate(payload)
   end
 end
--- test/trajectory_test.rb
+++ test/trajectory_test.rb
@@ -5,9 +5,21 @@ class TrajectoryTest < Service::TestCase
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
 
+  def test_raise_config_error_without_api_key
+    assert_raise Service::ConfigurationError do
+      svc = service({}, payload)
+      svc.receive_push
+    end
+
+    assert_raise Service::ConfigurationError do
+      svc = service({}, payload)
+      svc.receive_pull_request
+    end
+  end
+
   def test_push
     @stubs.post '/api/payloads?api_key=test_api_key' do |env|
-      assert_equal 'application/json', env[:request_headers]['Content-Type']
+      confirm_trajectory_receives_request(env)
       [200, {}, '']
     end
 
@@ -17,7 +29,24 @@ class TrajectoryTest < Service::TestCase
     @stubs.verify_stubbed_calls
   end
 
+  def test_pull_request
+    @stubs.post '/api/payloads?api_key=test_api_key' do |env|
+      confirm_trajectory_receives_request(env)
+      [200, {}, '']
+    end
+
+    svc = service({'api_key' => 'test_api_key'}, payload)
+    svc.receive_pull_request
+
+    @stubs.verify_stubbed_calls
+  end
+
   def service(*args)
     super Service::Trajectory, *args
+  end
+
+  def confirm_trajectory_receives_request(env)
+    assert_equal 'application/json', env[:request_headers]['Content-Type']
+    assert_equal 'https://www.apptrajectory.com/api/payloads?api_key=test_api_key', env[:url].to_s
   end
 end
