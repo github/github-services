7162871afd03c652a99b5481bcced9a66b41c3cf
Gurinder Gulati <gurinder.gulati@countersoft.com>
2012-10-23 11:14:10 +0100

    removed username as we don't need it.

--- services/gemini.rb
+++ services/gemini.rb
@@ -2,10 +2,6 @@ class Service::Gemini < Service
 string   :server_url, :api_key
 
   def receive_push
-  
-    if data['username'].to_s.empty?
-      raise_config_error "Username is missing"
-	end
 	
 	if data['server_url'].to_s.empty?
       raise_config_error "Server URL is missing"
@@ -16,7 +12,7 @@ string   :server_url, :api_key
 	end
 	
     # Sets this basic auth info for every request. GitHub user and Gemini API Key. 
-    http.basic_auth(data['username'], data['api_key'])
+    http.basic_auth(data['api_key'], data['api_key'])
 
     # Every request sends JSON.
     http.headers['Content-Type'] = 'application/json'
