cd5565c93899fc6f90727ef6c0af4f2a489b3a61
Brett Hellman <brett@hall-inc.com>
2012-11-01 16:32:50 -0700

    Supportors and Maintainers

--- services/hall.rb
+++ services/hall.rb
@@ -3,6 +3,10 @@ class Service::Hall < Service
   default_events :commit_comment, :gollum, :issues, :issue_comment, :pull_request, :push
   string :room_token
 
+  # Supportors and Maintainers
+  maintained_by :github => 'bhellman1'
+  supported_by :web => 'https://hallcom.uservoice.com/', :email => 'contact@hall-inc.com', :twitter => 'hall'
+
   def receive_push
     raise_config_error "Missing room token" if data['room_token'].to_s.empty?
     room_token = data['room_token'].to_s
