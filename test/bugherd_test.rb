12d93239b6721efb584a01182b4b76ccab57cb36
Vincent Brendel <vincent@192-168-1-43.tpgi.com.au>
2012-11-26 13:49:13 +1100

    individual tests per method (per technoweenie's suggestion)

--- test/bugherd_test.rb
+++ test/bugherd_test.rb
@@ -29,6 +29,54 @@ class BugHerdTest < Service::TestCase
     svc.receive_issue_comment
   end
 
+  def test_issues
+    @stubs.post "/custom" do |env|
+      assert_equal 'www.example.com', env[:url].host
+      assert_equal 'application/x-www-form-urlencoded',
+        env[:request_headers]['content-type']
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
+    svc.receive_issues
+
+    @stubs.post "/github_web_hook/KEY" do |env|
+      assert_equal 'www.bugherd.com', env[:url].host
+      assert_equal 'application/x-www-form-urlencoded',
+        env[:request_headers]['content-type']
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'url' => '', 'project_key' => 'KEY'}, payload
+    svc.receive_issues
+  end
+
+  def test_issue_comment
+    @stubs.post "/custom" do |env|
+      assert_equal 'www.example.com', env[:url].host
+      assert_equal 'application/x-www-form-urlencoded',
+        env[:request_headers]['content-type']
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
+    svc.receive_issue_comment
+
+    @stubs.post "/github_web_hook/KEY" do |env|
+      assert_equal 'www.bugherd.com', env[:url].host
+      assert_equal 'application/x-www-form-urlencoded',
+        env[:request_headers]['content-type']
+      [200, {}, '']
+    end
+
+    svc = service :push,
+      {'url' => '', 'project_key' => 'KEY'}, payload
+    svc.receive_issue_comment
+  end
+
   def service(*args)
     super Service::BugHerd, *args
   end
