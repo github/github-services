d54bb576a6a11eeba0d2195f412157e2f6962278
Vincent Brendel <vincent@192-168-1-43.tpgi.com.au>
2012-11-28 16:58:49 +1100

    simplify bugherd callback and test cases

--- services/bugherd.rb
+++ services/bugherd.rb
@@ -4,16 +4,10 @@ class Service::BugHerd < Service
   white_list :project_key
 
   def receive_push
-    if data['url'].present?
-      url = data['url']
-    else
-      url = "http://www.bugherd.com/github_web_hook/#{data['project_key']}"
-    end
-    http_post url,
-      :payload => JSON.generate(payload)
+    url = "http://www.bugherd.com/github_web_hook/#{data['project_key']}"
+    http_post url, :payload => JSON.generate(payload)
   end
 
   alias receive_issues receive_push
   alias receive_issue_comment receive_push
-
 end
--- test/bugherd_test.rb
+++ test/bugherd_test.rb
@@ -4,17 +4,6 @@ class BugHerdTest < Service::TestCase
   end
 
   def test_push
-    @stubs.post "/custom" do |env|
-      assert_equal 'www.example.com', env[:url].host
-      assert_equal 'application/x-www-form-urlencoded',
-        env[:request_headers]['content-type']
-      [200, {}, '']
-    end
-
-    svc = service :push,
-      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
-    svc.receive_push
-
     @stubs.post "/github_web_hook/KEY" do |env|
       assert_equal 'www.bugherd.com', env[:url].host
       assert_equal 'application/x-www-form-urlencoded',
@@ -22,25 +11,11 @@ class BugHerdTest < Service::TestCase
       [200, {}, '']
     end
 
-    svc = service :push,
-      {'url' => '', 'project_key' => 'KEY'}, payload
+    svc = service :push, {'project_key' => 'KEY'}, payload
     svc.receive_push
-    svc.receive_issues
-    svc.receive_issue_comment
   end
 
   def test_issues
-    @stubs.post "/custom" do |env|
-      assert_equal 'www.example.com', env[:url].host
-      assert_equal 'application/x-www-form-urlencoded',
-        env[:request_headers]['content-type']
-      [200, {}, '']
-    end
-
-    svc = service :push,
-      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
-    svc.receive_issues
-
     @stubs.post "/github_web_hook/KEY" do |env|
       assert_equal 'www.bugherd.com', env[:url].host
       assert_equal 'application/x-www-form-urlencoded',
@@ -48,23 +23,11 @@ class BugHerdTest < Service::TestCase
       [200, {}, '']
     end
 
-    svc = service :push,
-      {'url' => '', 'project_key' => 'KEY'}, payload
+    svc = service :push, {'project_key' => 'KEY'}, payload
     svc.receive_issues
   end
 
   def test_issue_comment
-    @stubs.post "/custom" do |env|
-      assert_equal 'www.example.com', env[:url].host
-      assert_equal 'application/x-www-form-urlencoded',
-        env[:request_headers]['content-type']
-      [200, {}, '']
-    end
-
-    svc = service :push,
-      {'url' => 'http://www.example.com/custom', 'project_key' => 'KEY'}, payload
-    svc.receive_issue_comment
-
     @stubs.post "/github_web_hook/KEY" do |env|
       assert_equal 'www.bugherd.com', env[:url].host
       assert_equal 'application/x-www-form-urlencoded',
@@ -72,8 +35,7 @@ class BugHerdTest < Service::TestCase
       [200, {}, '']
     end
 
-    svc = service :push,
-      {'url' => '', 'project_key' => 'KEY'}, payload
+    svc = service :push, {'project_key' => 'KEY'}, payload
     svc.receive_issue_comment
   end
 
