29a2cba4e7e9ae89795bb801fb8dc0a1ef2d811c
Ken MacInnis <ken.macinnis@gmail.com>
2012-12-20 11:05:13 -0800

    adjust whitespace

--- test/flowdock_test.rb
+++ test/flowdock_test.rb
@@ -9,28 +9,28 @@ class FlowdockTest < Service::TestCase
   def test_push
     @tokens.to_s.split(",").each do |t|
       @stubs.post "/v1/github/#{t}" do |env|
-	assert_match /json/, env[:request_headers]['content-type']
-	assert_equal push_payload, JSON.parse(env[:body])
-	[200, {}, '']
+        assert_match /json/, env[:request_headers]['content-type']
+        assert_equal push_payload, JSON.parse(env[:body])
+        [200, {}, '']
       end
     end
 
     svc = service(
       {'token' => @tokens}, push_payload)
-    svc.receive_event
+      svc.receive_event
   end
 
   def test_token_sanitization
     @tokens.to_s.split(",").each do |t|
       @stubs.post "/v1/github/#{t}" do |env|
-	assert_equal payload, JSON.parse(env[:body])
-	[200, {}, '']
+        assert_equal payload, JSON.parse(env[:body])
+        [200, {}, '']
       end
     end
 
     svc = service(
       {'token' => " " + @tokens + " "}, payload)
-    svc.receive_event
+      svc.receive_event
   end
 
   def service(*args)
