41c29cb24291bbdec1e4ec647e05cc5f0bbb858a
rick <technoweenie@gmail.com>
2013-02-05 12:30:34 -0700

    temporarily set nodeci hook name to hubci
    /cc @ptit6sky

--- lib/services/nodeci.rb
+++ lib/services/nodeci.rb
@@ -1,6 +1,10 @@
 class Service::NodeCI < Service
   string :token
 
+  # backwards compatible change until we can migrate configured hooks on
+  # github.com
+  hook_name 'hubci'
+
   def receive_push
     http.ssl[:verify] = false
     http.headers['Content-Type'] = 'application/json'
