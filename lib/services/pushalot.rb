4fd2c95ee9496b537aa06fbf642195fb64a4b4cf
Maciej Olesiński <usz@MBP.local>
2013-03-23 12:45:41 +0100

    Fix Pushalot service to accept untouched payload

--- lib/services/pushalot.rb
+++ lib/services/pushalot.rb
@@ -1,39 +1,23 @@
 class Service::Pushalot < Service
   string :authorization_token
 
+  url "https://pushalot.com"
+  logo_url "https://pushalot.com/content/images/favicon.png"
+  maintained_by :github => 'molesinski'
+  supported_by :web => 'https://pushalot.com/support'
+
   def receive_push
-    if !payload["commits"].any?
-      return
+    res = http_post "https://pushalot.com/api/githubhook",
+      :authorizationToken => authorization_token,
+      :payload => payload.to_json 
+
+    if res.status != 200
+      raise_config_error
     end
+  end
 
-    if !data["authorization_token"] or data["authorization_token"].length != 32
-      raise_config_error "Invalid Pushalot authorization token."
-    end
-
-    url = URI.parse("https://pushalot.com/api/sendmessage")
-
-    commits = payload["commits"].length
-    repo = payload["repository"]["url"].split("/")[-2 .. -1].join("/")
-    latest_message = payload["commits"].last["message"].split("\n").first
-    if latest_message.length > 1000
-      latest_message = latest_message[0 ... 997] + "..."
-    end
-    latest_url = shorten_url(payload["commits"].last["url"])
-
-    if commits == 1
-      title = "#{payload["pusher"]["name"]} pushed to #{repo}"
-      message = latest_message
-    else
-      title = "#{payload["pusher"]["name"]} pushed #{commits} " +
-        "commit#{commits == 1 ? '' : 's'} to #{repo}"
-      message = "Latest: #{latest_message}"
-    end
-
-    http_post url.to_s,
-      :AuthorizationToken => data["authorization_token"],
-      :Title => title,
-      :Body => message,
-      :Link => latest_url,
-      :LinkTitle => "View commit on GitHub"
+  def authorization_token
+    data["authorization_token"].to_s.strip
   end
 end
+
--- test/pushalot_test.rb
+++ test/pushalot_test.rb
@@ -3,27 +3,39 @@ require File.expand_path('../helper', __FILE__)
 class PushalotTest < Service::TestCase
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
+    @svc   = service(data, payload)
   end
 
-  def test_push
-    svc = service({"authorization_token" => "be82304d88d74eb884e384a98a282b8a"}, payload)
+  def test_reads_token_from_data
+    assert_equal "be82304d88d74eb884e384a98a282b8a", @svc.authorization_token
+  end
 
-    def svc.shorten_url(*args)
-      "short"
-    end
+  def test_strips_whitespace_from_token
+    svc = service({'authorization_token' => 'be82304d88d74eb884e384a98a282b8a  '}, payload)
+    assert_equal 'be82304d88d74eb884e384a98a282b8a', svc.authorization_token
+  end
 
-    @stubs.post "/api/sendmessage" do |env|
-      assert_equal "pushalot.com", env[:url].host
+  def test_posts_payload
+    @stubs.post '/api/githubhook' do |env|
+      assert_equal 'https', env[:url].scheme
+      assert_equal 'pushalot.com', env[:url].host
       data = Rack::Utils.parse_query(env[:body])
-      assert_equal "be82304d88d74eb884e384a98a282b8a", data["AuthorizationToken"]
-      [200, {}, '']
+      assert_equal "be82304d88d74eb884e384a98a282b8a", data["authorizationToken"]
+      assert_equal payload, JSON.parse(data['payload'])
     end
 
-    svc.receive_push
+    @svc.receive_push
   end
+
+private
 
   def service(*args)
     super Service::Pushalot, *args
   end
-end
+
+  def data
+    { 'authorization_token' => 'be82304d88d74eb884e384a98a282b8a' }
+  end
+
+end 
 
