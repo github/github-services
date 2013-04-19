7965575cb7624b8f4f4af3e6953db64e8c909863
Scott Richq <scott.rich.nc@gmail.com>
2013-04-19 15:54:54 +0200

    fixing authentication flow to work with WebSphere and proxy server
    Conflicts:
    	lib/services/rational_team_concert.rb

--- lib/services/rational_team_concert.rb
+++ lib/services/rational_team_concert.rb
@@ -3,6 +3,34 @@ class Service::RationalTeamConcert < Service
 	password :password
 	boolean :basic_authentication
 	white_list :server_url, :username, :basic_authentication
+	attr_accessor :cookies
+
+  # Public: Lazily loads the Faraday::Connection for the current Service
+  # instance.
+  #
+  # @override for debugging purposes, insert a Logger
+  #
+  # options - Optional Hash of Faraday::Connection options.
+  #
+  # Returns a Faraday::Connection instance.
+  def http(options = {})
+    @http ||= begin
+      self.class.default_http_options.each do |key, sub_options|
+        sub_hash = options[key] ||= {}
+        sub_options.each do |sub_key, sub_value|
+          sub_hash[sub_key] ||= sub_value
+        end
+      end
+      options[:ssl][:ca_file] ||= ca_file
+
+      Faraday.new(options) do |b|
+        b.use Faraday::Response::Logger
+        b.use HttpReporter, self
+        b.request :url_encoded
+        b.adapter *(options[:adapter] || :net_http)
+      end
+    end
+  end
 
 	def receive_push
 		checkSettings
@@ -33,23 +61,57 @@ class Service::RationalTeamConcert < Service
 	def form_based_authentification
 		res= http_get '%s/authenticated/identity' % data['server_url']
 		if not 'authrequired'.eql? res.headers['X-com-ibm-team-repository-web-auth-msg']
-			raise_config_error "Invalid authentication url. The response did not include a X-com-ibm-team-repository-web-auth-msg header"
+			# Expect one follow for WAS login
+			if res.env[:status] == 302
+				captureCookies res
+				http.headers['Cookie']= cookieString
+				res = http_get res.env[:response_headers][:location]
+				if not 'authrequired'.eql? res.headers['X-com-ibm-team-repository-web-auth-msg']
+					raise_config_error "Invalid authentication url. The response did not include a X-com-ibm-team-repository-web-auth-msg header"
+				end
+			else
+				raise_config_error "Invalid authentication url. The response did not include a X-com-ibm-team-repository-web-auth-msg header"
+			end
 		end
-		http.headers['Cookie']= res.headers['set-cookie']
+		http.headers['Cookie']= captureCookies res
 		http.headers['Content-Type']= 'application/x-www-form-urlencoded'
 
 		res= http_post '%s/authenticated/j_security_check' % data['server_url'], 
 					   Faraday::Utils.build_nested_query(http.params.merge(:j_username => data['username'], :j_password => data['password'])) 
 
 		if 'authrequired'.eql? res.headers['X-com-ibm-team-repository-web-auth-msg']
-			raise_config_error "Invalid Username or Password"
+			raise_config_error 'Invalid Username or Password %s' % res.env[:url]
 		end
-
+	
+		http.headers['Cookie']= captureCookies res
 		http.headers['Content-Type']= ''
 		res= http_get '%s/authenticated/identity' % data['server_url']
-		http.headers['Cookie']= res.headers['set-cookie']
+		captureCookies res
 	end
-
+	
+	def captureCookies (response)
+		@cookies ||= Hash.new
+		setstring = response.headers['set-cookie']
+		if setstring
+	    	setstring.split(/, (?=[\w]+=)/).each { | cookie |
+   		 		# trim off the cookie domain and update info
+        		cookiepair = cookie.split('; ')[0];
+        		# split the key and value
+        		cookieparts = cookiepair.split('=')
+        		cookies[cookieparts[0]] = cookiepair[cookieparts[0].size+1..-1]
+	    	}
+        end
+    	return cookieString
+	end
+	
+	def cookieString
+		result = ''
+		cookies.each { |key, value| 
+			result += key + '=' + value + ";"
+		}
+		return result
+	end
+	
 	def commit_changes
 		http.headers['Content-Type']= 'application/json'
 		http.headers['oslc-core-version']= '2.0'
@@ -79,13 +141,21 @@ class Service::RationalTeamConcert < Service
 	end
 
 	def get_work_item (work_item_number)
+		http.headers['Cookie']= cookieString
 		res= http_get "%s/resource/itemName/com.ibm.team.workitem.WorkItem/%s?oslc.properties=oslc:discussedBy" % [ data['server_url'], work_item_number ]  
+		# Expect one follow for WAS login
+		if res.env[:status] == 302
+			captureCookies res
+			http.headers['Cookie']= cookieString
+			res = http_get res.env[:response_headers][:location]
+		end		
 		return res.body
 	end
 
 	def new_work_item (commit, work_item_type)
 		url= "%s/oslc/contexts/%s/workitems/%s" % [data['server_url'], data['project_area_uuid'], work_item_type]
 		work_item= { 'dcterms:title' => commit['message']}
+		http.headers['Cookie']= cookieString
 		res= http_post url, generate_json(work_item)
 		raise_config_error "Work item was not created. Make sure that its possible to create work items with no additional required fields" unless res.status == 201
 		return res.body
@@ -94,14 +164,16 @@ class Service::RationalTeamConcert < Service
 	def post_comment(work_item, comment_body)
 		comment_url= get_comment_url work_item
 		comment= { 'dcterms:description' => comment_body }
+		http.headers['Cookie']= cookieString
 		res= http_post comment_url, generate_json(comment)
 		raise_config_error "Not possible to add comments with the current setup" unless res.status == 201
 	end
 
 	def get_comment_url (work_item) 
 		answer= JSON.parse(work_item)
+		raise_config_error "Invalid OSLC response. Unable to parse the work item: %s" % work_item unless answer
 		discussedBy= answer['oslc:discussedBy']
-		raise_config_error "Invalid OSLC response. Expected to receive oslc:discussedBy in the response" unless discussedBy
+		raise_config_error "Invalid OSLC response. Expected to receive oslc:discussedBy in the response %s" % work_item unless discussedBy
 		return "#{discussedBy['rdf:resource']}/oslc:comment"
 	end
 end
