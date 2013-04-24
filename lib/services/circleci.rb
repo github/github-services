25c30cb4fce06c83a0b78eba0fe118983b7e0399
rick <technoweenie@gmail.com>
2013-04-24 11:44:51 -0600

    circleci spacing issues

--- lib/services/circleci.rb
+++ lib/services/circleci.rb
@@ -1,22 +1,19 @@
 class Service::Circleci < Service
-
   url "https://circleci.com"
   logo_url "https://circleci.com/favicon.ico"
 
   maintained_by :github => 'circleci'
   supported_by  :web => 'https://circleci.com/about',
-  :email => 'sayhi@circleci.com'
+    :email => 'sayhi@circleci.com'
 
   default_events Service::ALL_EVENTS
 
-
   def receive_event
-
     http.headers['content-type'] = 'application/x-www-form-urlencoded'
-    http_post circleci_url, "payload" => JSON.generate(payload) , "event_type" =>  JSON.generate({ :event_type => self.event })
-    
+    http_post circleci_url,
+      "payload" => generate_json(payload),
+      "event_type" =>  generate_json(:event_type => event)
   end
-
 
   private
 
@@ -24,3 +21,4 @@ class Service::Circleci < Service
     "https://circleci.com/hooks/github" 
   end
 end
+
--- test/circleci_test.rb
+++ test/circleci_test.rb
@@ -1,11 +1,9 @@
 require File.expand_path('../helper', __FILE__)
 class CircleciTest < Service::TestCase
-  
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
 
-  
   # currently supported events
 
   # commit_comment create delete download follow fork fork_apply gist gollum
@@ -19,7 +17,7 @@ class CircleciTest < Service::TestCase
   def test_create
     post_to_service(:create)
   end
-    
+
   def test_delete
     post_to_service(:delete)
   end
@@ -64,8 +62,6 @@ class CircleciTest < Service::TestCase
     post_to_service(:public)
   end
 
-
-  
   def test_push
     post_to_service(:push)
   end
@@ -90,29 +86,26 @@ class CircleciTest < Service::TestCase
   def test_supported_events
     assert_equal Service::Circleci.supported_events.sort , Service::ALL_EVENTS.sort
   end
-  
 
   private
-  
+
   def service(*args)
     super Service::Circleci, *args
   end
-  
+
   def post_to_service(event_name)
     assert Service::ALL_EVENTS.include? event_name.to_s
-    svc = service(event_name, {'token' => 'abc'},payload)
+    svc = service(event_name, {'token' => 'abc'}, payload)
 
     @stubs.post "/hooks/github" do |env|
       body = CGI.parse env[:body]
       assert_match "https://circleci.com/hooks/github", env[:url].to_s
-      assert_match 'application/x-www-form-urlencoded', env[:request_headers]['content-type']      
+      assert_match 'application/x-www-form-urlencoded', env[:request_headers]['content-type']
       assert_equal payload, JSON.parse(body["payload"].to_s)
       assert_equal event_name.to_s, JSON.parse(body["event_type"].to_s)["event_type"]
     end
-    
+
     svc.receive_event
-
   end
-
 end
 
