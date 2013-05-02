7fc1265ebadc6487efa993f86aa726c6b2cb43cf
rick <technoweenie@gmail.com>
2013-05-02 14:57:54 -0600

    update to use Service::HttpPost

--- lib/services/simperium.rb
+++ lib/services/simperium.rb
@@ -1,4 +1,4 @@
-class Service::Simperium < Service
+class Service::Simperium < Service::HttpPost
   string :app_id, :token
 
   white_list :app_id
@@ -9,8 +9,10 @@ class Service::Simperium < Service
 
   url "https://simperium.com"
   logo_url "https://simperium.com/media/images/simperium_logo_black_sm.png"
+
   maintained_by :github => 'fredrocious',
     :twitter => '@fredrocious'
+
   supported_by :web => 'https://simperium.com/contact',
     :email => 'help@simperium.com',
     :twitter => '@simperium'
@@ -30,15 +32,13 @@ class Service::Simperium < Service
       raise_config_error 'Invalid token.'
     end
 
-    http.url_prefix = "https://api.simperium.com/1/#{appid}/#{event.to_s}/i/"
-    http.headers['X-Simperium-Token'] = token
+    wrap_http_errors do
+      url = set_url "https://api.simperium.com/1/#{appid}/#{event.to_s}/i/"
+      http.headers['X-Simperium-Token'] = token
 
-    if event.to_s =~ /push/
-        payload['commits'].each do |commit|
-          http_post "#{commit['id']}", generate_json(commit)
-        end
-    else
-        http_post "#{Time.now.to_i}", generate_json(payload)
+      body = encode_body
+      http_post(url, body)
     end
   end
 end
+
--- test/simperium_test.rb
+++ test/simperium_test.rb
@@ -1,33 +1,36 @@
 require File.expand_path('../helper', __FILE__)
 
 class SimperiumTest < Service::TestCase
-
-  def setup
-    @stubs = Faraday::Adapter::Test::Stubs.new
-    @test_app_id = "sample-app-name"
-    @test_token = "0123456789abcde"
-  end
+  include Service::HttpTestMethods
 
   def test_push
+    test_app_id = "sample-app-name"
+    test_token = "0123456789abcde"
+
+    data = {
+      'app_id' => test_app_id,
+      'token' => test_token
+    }
+
     payload = {'commits'=>[{'id'=>'test'}]}
-    @stubs.post "/1/#{@test_app_id}/push/i/test" do |env|
-      commit = JSON.parse(env[:body])
+    @stubs.post "/1/#{test_app_id}/push/i" do |env|
+      body = JSON.parse(env[:body])
 
       assert_equal env[:url].host, "api.simperium.com"
-      assert_equal env[:request_headers]['X-Simperium-Token'], @test_token
-      assert_equal 'test', commit['id']
+      assert_equal env[:request_headers]['X-Simperium-Token'], test_token
+      assert_equal 'test', body['payload']['commits'][0]['id']
+      assert_match 'guid-', body['guid']
+      assert_equal data, body['config']
+      assert_equal 'push', body['event']
       [200, {}, '']
     end
 
-    svc = service( {
-      'app_id' => @test_app_id,
-      'token' => @test_token
-    }, payload)
+    svc = service(data, payload)
     svc.receive_event
   end
 
-  def service(*args)
-    super Service::Simperium, *args
+  def service_class
+    Service::Simperium
   end
 end
 
