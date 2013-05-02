699315ca085b839c214207c1afa3d6c2d74c043d
risk danger olson <technoweenie@gmail.com>
2013-05-02 08:25:57 -0600

    extract an http helper

---
+++ lib/service/http_helper.rb
@@ -0,0 +1,54 @@
+class Service
+  module HttpHelper
+    HMAC_DIGEST = OpenSSL::Digest::Digest.new('sha1')
+
+    def set_url(url)
+      url = url.to_s
+      url.gsub! /\s/, ''
+
+      if url.empty?
+        raise_config_error "Invalid URL: #{url.inspect}"
+      end
+
+      if url !~ /^https?\:\/\//
+        url = "http://#{url}"
+      end
+
+      # set this so that basic auth is added,
+      # and GET params are added to the POST body
+      http.url_prefix = url
+
+      url
+    end
+
+    def set_body_signature(body, secret)
+      return if (secret = secret.to_s).empty?
+      http.headers['X-Hub-Signature'] =
+        'sha1='+OpenSSL::HMAC.hexdigest(HMAC_DIGEST, secret, body)
+    end
+
+    def original_body
+      {:payload => payload, :event => event.to_s, :config => data}
+    end
+
+    def encode_body(content_type = nil)
+      method = "encode_body_as_#{content_type}"
+      respond_to?(method) ? send(method) : default_encode_body
+    end
+
+    def default_encode_body
+      encode_body_as_json
+    end
+
+    def encode_body_as_json
+      http.headers['content-type'] = 'application/json'
+      generate_json(original_body)
+    end
+
+    def encode_body_as_form
+      http.headers['content-type'] = 'application/x-www-form-urlencoded'
+      Faraday::Utils.build_nested_query(
+        http.params.merge(:payload => generate_json(original_body)))
+    end
+  end
+end
--- lib/services/web.rb
+++ lib/services/web.rb
@@ -1,5 +1,5 @@
 class Service::Web < Service
-  HMAC_DIGEST = OpenSSL::Digest::Digest.new('sha1')
+  include HttpHelper
 
   string :url,
     # adds a X-Hub-Signature of the body content
@@ -15,39 +15,17 @@ class Service::Web < Service
   boolean :insecure_ssl # :(
 
   def receive_event
-    url = data['url'].to_s
-    url.gsub! /\s/, ''
+    url = set_url(data['url'])
 
-    if url.empty?
-      raise_config_error "Invalid URL: #{url.inspect}"
-    end
-
-    if url !~ /^https?\:\/\//
-      url = "http://#{url}"
-    end
-
-    # set this so that basic auth is added,
-    # and GET params are added to the POST body
-    http.url_prefix = url
     http.headers['X-GitHub-Event'] = event.to_s
 
     if data['insecure_ssl'].to_i == 1
       http.ssl[:verify] = false
     end
 
-    body = if data['content_type'] == 'json'
-      http.headers['content-type'] = 'application/json'
-      generate_json(payload)
-    else
-      http.headers['content-type'] = 'application/x-www-form-urlencoded'
-      Faraday::Utils.build_nested_query(
-        http.params.merge(:payload => generate_json(payload)))
-    end
+    body = encode_body(data['content_type'])
 
-    if !(secret = data['secret'].to_s).empty?
-      http.headers['X-Hub-Signature'] =
-        'sha1='+OpenSSL::HMAC.hexdigest(HMAC_DIGEST, secret, body)
-    end
+    set_body_signature(body, data['secret'])
 
     http_post url, body
   rescue Addressable::URI::InvalidURIError, Errno::EHOSTUNREACH
@@ -60,6 +38,14 @@ class Service::Web < Service
     end
   rescue EOFError
     raise_config_error "Invalid server response. Make sure the URL uses the correct protocol."
+  end
+
+  def original_body
+    payload
+  end
+
+  def default_encode_body
+    encode_body_as_form
   end
 end
 
