c7de0e4ea6e5f9ab4f433bef539f6a7ef0a2c6bd
SAPDshell <dshell@sap.com>
2013-05-16 14:00:01 -0600

    Update SAPDshell.rb

--- lib/services/SAPDshell.rb
+++ lib/services/SAPDshell.rb
@@ -1,9 +1,24 @@
-class Service::SAPD shell < Service
+class Service::SAPDshell < Service
   string   :dshell_url, :user_id
   password :password
   white_list :space_id, :user_id
 
+
+  url "http://dshell.saphana.com"
+  logo_url "http://www.sap.com/global/ui/images/global/sap-logo.png"
+
+  # SAPDshell on GitHub is pinged for any bugs with the Hook code.
+  maintained_by :github => 'SAPDshell'
+
+  # Support channels for user-level Hook problems (service failure,
+  # misconfigured
+  supported_by :web => 'http://scn.sap.com/community/developer-center/content',
+               :email => 'dshell@sap.com'
+        
+
   def receive_push
+    
+    # Check for settings
     if data['dshell_url'].to_s.empty?
       raise_config_error "SAP Dshell URL not set"
     end
@@ -13,101 +28,30 @@ class Service::SAPD shell < Service
     if data['password'].to_s.empty?
       raise_config_error "password not set"
     end
-
+    
+    # Sets this basic auth info for every request.
+    http.basic_auth(data['user_id'], data['password'])
+    
+    http.headers['Content-Type'] = 'application/json'
+    
+    # Uses this URL as a prefix for every request.
+    http.url_prefix = "https://"
+    
+    
     repository = payload['repository']['url'].to_s
-    commits = payload['commits'].collect{|c| Commit.new(c)}
-    issue_commits = sort_commits(commits)
-    issue_commits.sort.map do | issue, commits |
-      post(issue, repository, commits, branch.to_s)
+    dshell_url = data['dshell_url']
+    
+    call_url =  "{dshell_url}/?repo={repository}"
+      
+    res = http_post call_url 
+
+    if res.status < 200 || res.status > 299
+      raise_config_error "Failed with #{res.status}"
     end
 
-  end
-
-  def branch
-    return @branch if defined?(@branch)
-
-    matches = payload['ref'].match(/^refs\/heads\/(.*)$/)
-    @branch = matches ? matches[1] : nil
-  end
-
-  attr_writer :xmlrpc_client
-  def xmlrpc_client
-    @xmlrpc_client ||= begin
-                         uri = data['api_url'].to_s.sub('https://', "https://#{data['user_id'].to_s}:#{data['password'].to_s}@")
-                         client = XMLRPC::Client.new2(uri)
-                         # call for auth check
-                         client.call('backlog.getProjects')
-                         client
-                       rescue XMLRPC::FaultException
-                         raise_config_error "Invalid login details"
-                       rescue SocketError, RuntimeError
-                         raise_config_error "Invalid server url"
-                       end
-  end
-
-  def sort_commits(commits)
-    issue_commits = Hash.new{|k,v| k[v] = []}
-    commits.each do |commit|
-      commit.issue.each do |issue|
-        issue_commits[issue] << commit
-      end
-    end
-    return issue_commits
-  end
-
-  def post(issue, repository, commits, branch_name)
-    if commits.length == 0
-      return
-    end
-
-    branch_str = branch_name.empty? ? "" : "#{branch_name} at "
-    message = "pushed to #{branch_str}#{repository}\n\n"
-
-    commits.each do |commit|
-      comment = "#{message}#{commit.comment}"
-      begin
-        if commit.status
-          xmlrpc_client.call('backlog.switchStatus', {'key' => issue, 'statusId' => commit.status, 'comment' => comment})
-        else
-          xmlrpc_client.call('backlog.addComment', {'key' => issue, 'content' => comment})
-        end
-      rescue XMLRPC::FaultException
-        raise_config_error "failed post"
-      rescue RuntimeError
-        raise_config_error "failed post"
-      end
-    end
-  end
-
-  class Commit
-    attr_reader :status, :issue, :url, :id
-
-    def initialize(commit_hash)
-      @id = commit_hash['id'].to_s
-      @url = commit_hash['url'].to_s
-      @message = commit_hash['message'].to_s
-      @status = nil
-      @issue = []
-
-      re_issue_key = /(?:\[\[)?(([A-Z0-9]+(?:_[A-Z0-9]+)*)-([1-9][0-9]*))(?:\]\])?/
-      temp = @message
-      while temp =~ re_issue_key
-        issue << $1
-        temp.sub!($1, '')
-      end
-
-      re_status = /(?:^|\s+?)(#fixes|#fixed|#fix|#closes|#closed|#close)(?:\s+?|$)/
-      while @message =~ re_status
-        switch = $1
-        @message.sub!(switch, '')
-        @status = (switch =~ /fix/) ? 3 : 4
-      end
-    end
-
-    def comment()
-      output = "#{@url}\n"
-      output += @message.strip
-      return output
-    end
-  end
+  rescue URI::InvalidURIError
+    raise_config_error "Invalid URL: #{call_url}"
+    
+    
+  
 end
