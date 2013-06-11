7599b952e035eb9e0a8760059bcb3f24165a2f0a
Thomas Cannon <tcannon@codemonkeydev.com>
2013-06-11 11:49:36 -0400

    Freckle Service Hook Sends full Payload
    The Freckle Service Hook now sends the full payload unmodified.

--- services/freckle.rb
+++ services/freckle.rb
@@ -2,21 +2,10 @@ class Service::Freckle < Service
   string :subdomain, :project, :token
 
   def receive_push
-    entries, subdomain, token, project =
-      [], data['subdomain'].strip, data['token'].strip, data['project'].strip
-
-    payload['commits'].each do |commit|
-      entries << {
-        :timestamp => commit["timestamp"],
-        :message => commit["message"].strip,
-        :url => commit['url'],
-        :project_name => project,
-        :author_email => commit['author']['email']
-      }
-    end
+    subdomain, token, project = data['subdomain'].strip, data['token'].strip, data['project'].strip
 
     http.headers['Content-Type'] = 'application/json'
     http_post "http://#{data['subdomain']}.letsfreckle.com/api/github/commits",
-      {:entries => entries, :token => data['token']}.to_json
+      {:payload => payload, :token => data['token'], :project => project}.to_json
   end
 end
--- test/freckle_test.rb
+++ test/freckle_test.rb
@@ -7,7 +7,7 @@ class FreckleTest < Service::TestCase
 
   def test_posts_with_2_entries
     data = call_service :push
-    assert_equal 3, data['entries'].size
+    assert_equal 3, data['payload']['commits'].size
   end
 
   def test_includes_auth_token
@@ -18,33 +18,34 @@ class FreckleTest < Service::TestCase
   def test_sends_entire_commit_message
     data = call_service :push
     assert_equal 'stub git call for Grit#heads test f:15 Case#1',
-      data['entries'][0]['message']
+      data['payload']['commits'][0]['message']
     assert_equal 'clean up heads test f:2hrs',
-      data['entries'][1]['message']
+      data['payload']['commits'][1]['message']
   end
 
   def test_includes_project_name
     data = call_service :push
     assert_equal 'Test Project',
-      data['entries'][0]['project_name']
+      data['project']
   end
 
   def test_includes_author_email_as_user
     data = call_service :push
+    puts data.to_json
     assert_equal 'tom@mojombo.com',
-      data['entries'][0]['author_email']
+      data['payload']['commits'][0]['author']['email']
   end
 
   def test_includes_commit_url
     data = call_service :push
     assert_equal 'http://github.com/mojombo/grit/commit/06f63b43050935962f84fe54473a7c5de7977325',
-      data['entries'][0]['url']
+      data['payload']['commits'][0]['url']
   end
 
   def test_includes_timestamp
     data = call_service :push
     assert_equal '2007-10-10T00:11:02-07:00',
-      data['entries'][0]['timestamp']
+      data['payload']['commits'][0]['timestamp']
   end
 
   def data
