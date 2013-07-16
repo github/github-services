c428a7b91769085e4a92c79348870ef2a4e97b09
jonbonazza@gmail.com <jonbonazza@gmail.com>
2013-07-16 19:06:19 -0400

    Added devaria service and docs

---
+++ lib/services/devaria.rb
@@ -0,0 +1,76 @@
+class Service::Devaria < Service::HttpPost
+	string :project_name, :username, :user_class_id
+	
+	white_list :project_name, :username, :user_class_id
+	
+	default_events :push, :member, :public, :issues, :gollum
+	
+	url "http://devaria.com"
+	maintained_by :github => 'jonbonazza'
+	
+	supported_by :web => 'http://www.createtank.com/contactUs/'
+	@@url_base = "http://localhost:8080/hooks"
+	def receive_push
+		username = required_config_value('username')
+		project = required_config_value('project_name')
+		url = @@url_base + "/push"
+		body = {'owner'=>username, 'project_name'=>project, 'payload'=>payload}
+		body = generate_json(body)
+		make_request(url, body)
+		print body
+	end
+	
+	def make_request(url, body)
+		wrap_http_errors do
+			url = set_url(url)
+			http.headers['content-type'] = 'application/json'
+			http_post url, body
+		end
+	end
+	
+	def receive_member
+		username = required_config_value('username')
+		project = required_config_value('project_name')
+		
+		url = @@url_base + "/member"
+		body = {'owner'=>username, 'project_name'=>project, 'payload'=>payload}
+		body = generate_json(body)
+		make_request(url, body)
+	end
+	
+	def receive_public
+		username = required_config_value('username')
+		project = required_config_value('project_name')
+		
+		url = @@url_base + "/public"
+		body = {'owner'=>username, 'project_name'=>project, 'payload'=>payload}
+		body = generate_json(body)
+		make_request(url, body)
+	end
+	
+	def receive_issues
+		username = required_config_value('username')
+		project = required_config_value('project_name')
+		
+		url = @@url_base + "/issues"
+		body = {'owner'=>username, 'project_name'=>project, 'payload'=>payload}
+		body = generate_json(body)
+		make_request(url, body)
+	end
+	
+	def receive_gollum
+		username = required_config_value('username')
+		project = required_config_value('project_name')
+		
+		url = @@url_base + "/gollum"
+		body = {'owner'=>username, 'project_name'=>project, 'payload'=>payload}
+		body = generate_json(body)
+		make_request(url, body)
+	end
+end
+
+def test
+	# encoding: UTF-8
+	svc = Service::Devaria.new(:push, {'username'=>'jonbonazza', 'project_name'=>'Testeroos', 'user_class_id'=>5629499534213120})
+	svc.receive_push
+end
