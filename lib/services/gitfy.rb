d0036992bb32a8004497c7b42a6217049a24d464
sarriagada <sarriagada87@gmail.com>
2013-08-02 11:45:46 -0300

    Update gitfy.rb

--- lib/services/gitfy.rb
+++ lib/services/gitfy.rb
@@ -1,12 +1,12 @@
 class Service::Gitfy < Service
-  default_events :pull_request
 
   url "http://www.hashdog.com"
   logo_url "http://gitfy-design.herokuapp.com/img/logo.png"
   supported_by :web => 'http://www.hashdog.com/#contact'
   
-  def receive_event
-
+  def receive_push
+    http_post "http://gitfy-design.herokuapp.com/dashboard",
+      :payload => generate_json(payload)
   end
 
 end
