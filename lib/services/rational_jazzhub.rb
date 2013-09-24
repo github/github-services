f000fe462ae6e24986259bcbdbc94a894b29899f
Corey Donohoe <atmos@atmos.org>
2013-09-24 13:51:13 -0700

    formatting

--- lib/services/rational_jazzhub.rb
+++ lib/services/rational_jazzhub.rb
@@ -1,17 +1,17 @@
 class Service::RationalJazzHub < Service::HttpPost
-	string   :username
-	password :password
-	string :override_server_url
-	white_list :username
+  string   :username
+  password :password
+  string :override_server_url
+  white_list :username
 
-	def receive_push
-		username = required_config_value('username')
-		password = required_config_value('password')
-		override_server_url = data['override_server_url']
-		server_url = (override_server_url.nil? || override_server_url.empty? ? "https://hub.jazz.net/manage" : override_server_url)
-		post_url = "#{server_url}/processGitHubPayload?jazzhubUsername=#{username}&jazzhubPassword=#{password}"
-	    deliver post_url
-	end
+  def receive_push
+    username = required_config_value('username')
+    password = required_config_value('password')
+    override_server_url = data['override_server_url']
+    server_url = (override_server_url.nil? || override_server_url.empty? ? "https://hub.jazz.net/manage" : override_server_url)
+    post_url = "#{server_url}/processGitHubPayload?jazzhubUsername=#{username}&jazzhubPassword=#{password}"
+    deliver post_url
+  end
 
   def receive_push
     http.builder.response :logger
