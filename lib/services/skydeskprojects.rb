2bf4c0f4f89083af469c30e573a323cb04d84a13
SkyDeskProjects <support_projects@skydesk.jp>
2013-11-19 14:55:33 +0530

    Update skydeskprojects.rb
    Added Raw payload changes .

--- lib/services/skydeskprojects.rb
+++ lib/services/skydeskprojects.rb
@@ -1,15 +1,25 @@
-class Service::ZohoProjects < Service
+class Service::ZohoProjects < Service::HttpPost
   string :project_id, :token
+  
   white_list :project_id
 
-  def receive_push
-    res = http_post "https://projects.skydesk.jp/serviceHook",
-      :pId       => data['project_id'],
+  url "https://www.skydesk.jp"
+  logo_url "https://www.skydesk.jp/static/common/images/header/ci/ci_skydesk.gif"
+  
+  maintained_by :github => 'SkyDeskProjects'
+   
+  supported_by :web => 'www.skydesk.jp/en/contact/',
+    :email => 'support_projects@skydesk.jp'
+    
+   def receive_push
+    token = required_config_value('token')
+    
+    http.headers['Authorization'] = "Token #{token}"
+
+    url = "https://projects.skydesk.jp/serviceHook",
+      :pId => data['project_id'],
       :authtoken => data['token'],
-      :scope     => "projectsapi",
-      :payload   => generate_json(payload)
-    if res.status != 200
-      raise_config_error
-    end
+      :scope => "projectsapi"
+    deliver url
   end
 end
