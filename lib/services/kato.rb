6a46faa3547eb25eda8b4a3ef5e99d8785a26fa6
Agata Yurina <airelain@gmail.com>
2013-12-28 12:57:55 +0800

    Added ability to ignore some events on github side for kato.im

--- lib/services/kato.rb
+++ lib/services/kato.rb
@@ -1,6 +1,7 @@
 class Service::Kato < Service::HttpPost
   hook_name 'lechat'
   string :webhook_url
+  boolean :ignore_commits, :ignore_commit_comments, :ignore_issues, :ignore_issue_comments, :ignore_pull_requests, :ignore_pull_request_review_comments, :ignore_wiki_page_updates
 
   # include 'webhook_url' in the debug logs
   white_list :webhook_url
@@ -14,6 +15,14 @@ class Service::Kato < Service::HttpPost
   supported_by :email => 'support@kato.im'
 
   def receive_event
+    ignore_flag = case event.to_s
+      when 'push' then 'ignore_commits'
+      when 'gollum' then 'ignore_wiki_page_updates'
+      when 'issues' then 'ignore_issues'
+      else 'ignore_'+event.to_s+'s'
+    end
+    return if data[ignore_flag]
+
     webhook_url = required_config_value('webhook_url')
 
     res = deliver webhook_url
