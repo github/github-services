ad96b9710eccb2e4531bc2964399980907ebf65f
Ivan Žužak <ivan.zuzak@github.com>
2014-01-13 10:27:41 +0100

    add deployment events

--- lib/service.rb
+++ lib/service.rb
@@ -79,7 +79,7 @@ class Service
   ALL_EVENTS = %w[
     commit_comment create delete download follow fork fork_apply gist gollum
     issue_comment issues member public pull_request push team_add watch
-    pull_request_review_comment status release
+    pull_request_review_comment status release deployment deployment_status
   ].sort
 
   class << self
