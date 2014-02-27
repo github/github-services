812bce8f334a861829c98315c6148e5b1e55ff1b
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-02-27 02:19:45 +0200

    Update tests.

--- test/pushbullet_test.rb
+++ test/pushbullet_test.rb
@@ -4,7 +4,10 @@ class PushbulletTest < Service::TestCase
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
 
-    @options = {"api_key" => "a", "device_iden" => "hi"}
+    @options = {
+      "api_key" => "e4f235a3851g396d801772b504898140",
+      "device_iden" => "udhfWdjz4gRNO0Aq"
+    }
   end
 
   def test_push
@@ -12,10 +15,12 @@ class PushbulletTest < Service::TestCase
       check_env env
 
       expected = {
-        "device_iden"=>"hi",
+        "device_iden"=>"udhfWdjz4gRNO0Aq",
         "type"=>"note",
-        "title"=>"rtomayko pushed 3 commits",
-         "body"=>"Repo: mojombo/grit\nLatest: add more comments throughout" }
+        "title"=>"rtomayko pushed 3 commits...",
+        "body"=>
+        "To mojombo/grit:\nL[grit/master] stub git call for Grit#heads test f:15 Case#1 - Tom Preston-Werner\n[grit/master] clean up heads test f:2hrs - Tom Preston-Werner\n[grit/master] add more comments throughout - Tom Preston-Werner"
+      }
 
       assert_equal expected, Faraday::Utils.parse_query(env[:body])
 
@@ -30,10 +35,10 @@ class PushbulletTest < Service::TestCase
       check_env env
 
       expected = {
-        "device_iden"=>"hi",
+        "device_iden"=>"udhfWdjz4gRNO0Aq",
         "type"=>"note",
-        "title"=>"mojombo opened issue #5",
-        "body"=>"Repo: mojombo/grit\nIssue: \"booya\"\nboom town"
+        "title"=>"defunkt opened issue #5",
+        "body"=>"In mojombo/grit: \"booya\"\nboom town"
       }
 
       assert_equal expected, Faraday::Utils.parse_query(env[:body])
@@ -49,10 +54,10 @@ class PushbulletTest < Service::TestCase
       check_env env
 
       expected = {
-        "device_iden"=>"hi",
+        "device_iden"=>"udhfWdjz4gRNO0Aq",
         "type"=>"note",
-        "title"=>"foo opened pull request #5",
-        "body"=>"Repo: mojombo/grit\nPull Request: \"booya\"\nboom town"
+        "title"=>"defunkt opened pull request #5",
+        "body"=>"In mojombo/grit: \"booya\" (master...feature)\nboom town"
       }
 
       assert_equal expected, Faraday::Utils.parse_query(env[:body])
@@ -66,7 +71,7 @@ class PushbulletTest < Service::TestCase
   def check_env(env)
       assert_equal 'https', env[:url].scheme
       assert_equal "api.pushbullet.com", env[:url].host
-      assert_equal "Basic YTo=", env[:request_headers]["Authorization"]
+      assert_equal "Basic ZTRmMjM1YTM4NTFnMzk2ZDgwMTc3MmI1MDQ4OTgxNDA6", env[:request_headers]["Authorization"]
   end
 
   def service(*args)
