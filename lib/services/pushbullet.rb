38063874cfec61ce11bb4ed91c1e11d0d20baad1
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-03-04 19:27:37 +0200

    Switch Pushbullet API to handle payloads.

--- lib/services/pushbullet.rb
+++ lib/services/pushbullet.rb
@@ -1,11 +1,10 @@
-class Service::Pushbullet < Service
-
+class Service::Pushbullet < Service::HttpPost
   string :api_key, :device_iden
 
   default_events :push, :issues, :pull_request
 
   url "https://www.pushbullet.com/"
-  logo_url "https://lh3.ggpht.com/hlxRPX7B5J28cgGAZcovaT-7wLimLi0wPi7dSI6udH5NGI58WTBezGgpJyIepZhBRp4=w500"
+  logo_url "https://www.pushbullet.com/img/header-logo.png"
 
   maintained_by :github => 'tuhoojabotti',
     :twitter => 'tuhoojabotti',
@@ -14,77 +13,14 @@ class Service::Pushbullet < Service
   supported_by :web => 'https://www.pushbullet.com/help',
     :email => 'hey@pushbullet.com'
 
-  def receive_push
-    check_config
-
-    return unless commits.any?
-
-    if commits.length == 1
-      title = "#{pusher_name} pushed to #{name_with_owner}"
-      message = commit_messages.first
-    else
-      title = "#{pusher_name} pushed #{commits.length} commits..."
-      message = "To #{name_with_owner}:\nL#{commit_messages.join("\n")}"
+  def receive_event
+    unless required_config_value('api_key').match(/^[A-Za-z0-9]{32}$/)
+      raise_config_error "Invalid api key."
+    end
+    unless required_config_value('device_iden').match(/^[A-Za-z0-9]{16}$/)
+      raise_config_error "Invalid device iden."
     end
 
-    push_message title, message
-  end
-
-  def receive_issue
-    check_config
-
-    p = self.class.objectify payload
-    repo = p.repository
-    body = truncate_too_long issue.body, 300
-
-    title = "%s %s issue #%d" % [p.sender.login, p.action, issue.number]
-    msg = "In %s/%s: \"%s\"\n%s" % [repo.owner.login, repo.name, issue.title,
-      body]
-
-    push_message title, msg
-
-  end
-
-  def receive_pull_request
-    check_config
-
-    p = self.class.objectify payload
-    repo = p.repository
-    body = truncate_too_long pull.body, 200
-
-    base_ref = pull.base.label.split(':').last
-    head_ref = pull.head.label.split(':').last
-    head_ref = pull.head.label if head_ref == base_ref
-
-    title = "%s %s pull request #%d" % [p.sender.login, p.action, pull.number]
-    msg = "In %s/%s: \"%s\" (%s...%s)\n%s" % [repo.owner.login, repo.name,
-      pull.title, base_ref, head_ref, body]
-
-    push_message title, msg
-  end
-
-  private
-
-  def check_config
-    @api_key = data["api_key"].to_s
-    @iden = data["device_iden"].to_s
-    raise_config_error "Invalid or missing api key." unless @api_key.match(/\A[a-zA-Z0-9]{32}\z/)
-    raise_config_error "Invalid or missing device iden." unless @iden.match(/\A[a-zA-Z0-9]{16}\z/)
-  end
-
-  def push_message(title, message)
-    # set api key
-    http.basic_auth(@api_key, "")
-
-    # call api
-    http_post "https://api.pushbullet.com:443/api/pushes",
-      :device_iden => @iden,
-      :type => "note",
-      :title => title,
-      :body => message
-  end
-
-  def truncate_too_long(str, len)
-    str.length > len ? str[0..len].gsub(/\s\w+\s*$/, '...') : str
+    deliver "https://webhook.pushbullet.com:443/github"
   end
 end
--- test/pushbullet_test.rb
+++ test/pushbullet_test.rb
@@ -11,67 +11,29 @@ class PushbulletTest < Service::TestCase
   end
 
   def test_push
-    @stubs.post "/api/pushes" do |env|
-      check_env env
-
-      expected = {
-        "device_iden"=>"udhfWdjz4gRNO0Aq",
-        "type"=>"note",
-        "title"=>"rtomayko pushed 3 commits...",
-        "body"=>
-        "To mojombo/grit:\nL[grit/master] stub git call for Grit#heads test f:15 Case#1 - Tom Preston-Werner\n[grit/master] clean up heads test f:2hrs - Tom Preston-Werner\n[grit/master] add more comments throughout - Tom Preston-Werner"
-      }
-
-      assert_equal expected, Faraday::Utils.parse_query(env[:body])
-
-      [200, {}, '']
-    end
-
-    service(:push, @options, payload).receive_push
+    stub_request "push"
+    service(:push, @options, payload).receive_event
   end
 
   def test_issue
-    @stubs.post "/api/pushes" do |env|
-      check_env env
-
-      expected = {
-        "device_iden"=>"udhfWdjz4gRNO0Aq",
-        "type"=>"note",
-        "title"=>"defunkt opened issue #5",
-        "body"=>"In mojombo/grit: \"booya\"\nboom town"
-      }
-
-      assert_equal expected, Faraday::Utils.parse_query(env[:body])
-
-      [200, {}, '']
-    end
-
-    service(:issue, @options, issues_payload).receive_issue
+    stub_request "issues"
+    service(:issues, @options, issues_payload).receive_event
   end
 
   def test_pull_request
-    @stubs.post "/api/pushes" do |env|
-      check_env env
-
-      expected = {
-        "device_iden"=>"udhfWdjz4gRNO0Aq",
-        "type"=>"note",
-        "title"=>"defunkt opened pull request #5",
-        "body"=>"In mojombo/grit: \"booya\" (master...feature)\nboom town"
-      }
-
-      assert_equal expected, Faraday::Utils.parse_query(env[:body])
-
-      [200, {}, '']
-    end
-
-    service(:pull_request, @options, pull_payload).receive_pull_request
+    stub_request "pull_request"
+    service(:pull_request, @options, pull_payload).receive_event
   end
 
-  def check_env(env)
+  def stub_request(event)
+    @stubs.post "/github" do |env|
       assert_equal 'https', env[:url].scheme
-      assert_equal "api.pushbullet.com", env[:url].host
-      assert_equal "Basic ZTRmMjM1YTM4NTFnMzk2ZDgwMTc3MmI1MDQ4OTgxNDA6", env[:request_headers]["Authorization"]
+      assert_equal "webhook.pushbullet.com", env[:url].host
+      body = JSON.parse(env[:body])
+      assert_equal event, body['event']
+      assert_equal @options, body['config']
+      [200, {}, '']
+    end
   end
 
   def service(*args)
