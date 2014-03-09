b92bdb8426bd9001043972a212a92aa5f5e94a5b
Noel Baron <noel@goodfellaz.com>
2014-03-09 11:35:43 -0700

    Added Support Requirements

--- lib/services/crocagile.rb
+++ lib/services/crocagile.rb
@@ -1,6 +1,12 @@
 class Service::Crocagile < Service
   string :project_key
 
+  url "https://www.crocagile.com/home"
+  logo_url "https://www.crocagile.com/_images/crocagile100x100t.png"
+  maintained_by :github => 'noelbaron'
+  supported_by :web => 'https://www.crocagile.com/home',
+    :email => 'support@crocagile.com'
+
   def receive_event
     raise_config_error "Please enter your Project Key (located via Project Settings screen)." if data['project_key'].to_s.empty?
     http.headers['Content-Type'] = 'application/json'
