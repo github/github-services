b485e6f48b53dff9eecf2b8956230058a6236143
Ben Waters <bsawyerwaters@gmail.com>
2014-04-18 13:47:17 -0400

    Splendid Bacon is no longer hosting. It is now self-hosted. I added a host url field to fix the problem. As long as the user has the app setup correctlty the hook will work.

--- lib/services/splendid_bacon.rb
+++ lib/services/splendid_bacon.rb
@@ -1,11 +1,11 @@
 class Service::SplendidBacon < Service
-  string :project_id, :token
+  string :host_url, :project_id, :token
   white_list :project_id
 
   def receive_push
     token = data['token']
     project_id = data['project_id']
-    http.url_prefix = 'https://splendidbacon.com'
+    http.url_prefix = 'http://{host_url}'
     http_post "/api/v1/projects/#{project_id}/github" do |req|
       req.params[:token] = token
       req.body = {:payload => generate_json(payload)}
