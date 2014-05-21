2b8a756679ea808220a4ebe01d2fc21b1b80c338
Wang Xu <xu@mc2.io>
2014-05-21 15:41:55 +0800

    implement visualops.io batch update API
    - update all affected app as listed
    - do not send req if branch do not listed
    - pass Tests
    
    Signed-off-by: Wang Xu <xu@mc2.io>

--- lib/services/visualops.rb
+++ lib/services/visualops.rb
@@ -1,14 +1,15 @@
 class Service::VisualOps < Service
-  string :app_id, :user_id, :watch_branch
+  string :username, :app_list
   password :consumer_token
 
-  white_list :app_id, :user_id, :watch_branch
+  white_list :username, :app_list
 
   default_events :push
 
   url "http://www.visualops.io"
 
-  maintained_by :github => "gnawux", :twitter => "@gnawux" 
+  maintained_by :github => "gnawux", 
+      :twitter => "@gnawux" 
 
   supported_by :email => 'support@visualops.io'
 
@@ -19,31 +20,54 @@ class Service::VisualOps < Service
     assert_required_credentials :push
 
     # Update State
-    update_state :push
+    app = push_list(app_list)
+    if not app.empty?
+      update_apps(app)
+    end
   end
 
   private
 
   def update_states?
     return false if payload['commits'].size == 0
-    return false if branch_name != data['watch_branch']
     true
   end
 
-  def assert_required_credentials(event)
-    if consumer_token.empty?
-      raise_config_error "You need an authorization Token. See tips below."
+  def app_list
+    data['app_list'].split(',').map do |ab_pair|
+      (app, sep, bx) = ab_pair.strip.partition(":")
+      branch = bx.empty? ? "master" : bx
+      [app, branch]
     end
   end
 
-  def update_state(event, name, description)
-    http.url_prefix = "https://api.visualops.io/v1/apps"
-    http_post data['app_id'],
-      :user => data['user_id'],
-      :token => consumer_token
+  def push_list(apps)
+    apps.keep_if{|x| x[1] == branch_name}.map{|x| x[0]}
+  end
+
+  def assert_required_credentials(event)
+    if (consumer_token.empty? || username.empty?)
+      raise_config_error "You need a user ID and an authorization Token. See tips below."
+    end
+  end
+
+  def update_apps(apps)
+    http_post "https://api.visualops.io/v1/apps" do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        :user  => username,
+        :app   => apps,
+        :token => consumer_token,
+        :src   => branch_url
+        }.to_json
+    end
   end
 
   def consumer_token
     data['consumer_token'].to_s
+  end
+
+  def username
+    data['username'].to_s
   end
 end
