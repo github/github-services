26b9c5548729ad57b794bcb6d218b8bd01364fd5
Corey Donohoe <atmos@atmos.org>
2014-06-28 13:15:07 -0700

    this should be a symbol. /cc @n-dream

--- lib/services/codereviewhub.rb
+++ lib/services/codereviewhub.rb
@@ -7,5 +7,5 @@ class Service::Codereviewhub < Service::Web
 
   supported_by :email => 'contact@codereviewhub.com'
   maintained_by :github => 'codereviewhub'
-  default_events :pull_request, :issue_comment, :commit_comment, pull_request_review_comment
+  default_events :pull_request, :issue_comment, :commit_comment, :pull_request_review_comment
 end
