7607d7959e0dda30e0da4749c3774685a59edc2b
Corey Donohoe <atmos@atmos.org>
2014-08-18 13:02:47 -0700

    have opsworks support deployments too

--- lib/services/aws_ops_works.rb
+++ lib/services/aws_ops_works.rb
@@ -3,21 +3,46 @@ require 'aws/ops_works'
 class Service::AwsOpsWorks < Service::HttpPost
   self.title = 'AWS OpsWorks'
 
-  string     :stack_id,             # see StackId at http://docs.aws.amazon.com/opsworks/latest/APIReference/API_Stack.html
-             :app_id,               # see AppId at http://docs.aws.amazon.com/opsworks/latest/APIReference/API_App.html
+  string     :app_id,               # see AppId at http://docs.aws.amazon.com/opsworks/latest/APIReference/API_App.html
+             :stack_id,             # see StackId at http://docs.aws.amazon.com/opsworks/latest/APIReference/API_Stack.html
              :branch_name,          # see Revision at http://docs.aws.amazon.com/opsworks/latest/APIReference/API_Source.html
              :aws_access_key_id     # see AWSAccessKeyID at http://docs.aws.amazon.com/opsworks/latest/APIReference/CommonParameters.html
   password   :aws_secret_access_key
 
-  white_list :stack_id,
-             :app_id,
+  white_list :app_id,
+             :stack_id,
              :branch_name,
              :aws_access_key_id
 
+  default_events :push, :deployment
   url "http://docs.aws.amazon.com/opsworks/latest/APIReference/API_CreateDeployment.html"
 
   def receive_event
-    create_deployment if branch_name == required_config_value('branch_name')
+    case event.to_s
+    when 'deployment'
+      update_app(sha)
+      create_deployment
+    when 'push'
+      if branch_name == configured_branch_name
+        update_app_revision(configured_branch_name)
+        create_deployment
+      end
+    else
+      raise_config_error("The #{event} event is currently unsupported.")
+    end
+  end
+
+  def configured_branch_name
+    required_config_value('branch_name')
+  end
+
+  def sha
+    payload['sha'][0..7]
+  end
+
+  def update_app_revision(revision_name)
+    ops_works_client.update_app app_id: required_config_value('app_id'),
+                                app_source: { revision: revision_name }
   end
 
   def create_deployment
@@ -30,5 +55,4 @@ class Service::AwsOpsWorks < Service::HttpPost
     AWS::OpsWorks::Client.new access_key_id:     required_config_value('aws_access_key_id'),
                               secret_access_key: required_config_value('aws_secret_access_key')
   end
-
 end
