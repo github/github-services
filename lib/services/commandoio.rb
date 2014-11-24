91661b189b899ec1931c31b2341d63c04fc8d33f
Alex Williams <awilliams@alexwilliams.ca>
2014-11-24 10:53:05 +0000

    Small documentation update for Commandoio and fix to use the 'config_boolean_true' helper

--- lib/services/commandoio.rb
+++ lib/services/commandoio.rb
@@ -52,7 +52,7 @@ class Service::Commandoio < Service::HttpPost
 
     params.merge!(:server          => data['server'])             if data['server']
     params.merge!(:groups          => groups)                     unless groups.nil?
-    params.merge!(:halt_on_stderr  => data['halt_on_stderr'])     if data['halt_on_stderr']
+    params.merge!(:halt_on_stderr  => data['halt_on_stderr'])     if config_boolean_true?('halt_on_stderr')
     params.merge!(:notes           => CGI.escape(data['notes']))  if data['notes']
 
     http_post url, params
