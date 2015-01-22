c1a021a918dd6615763278baa297d85b03ea1674
Eric Lordahl <eric.lordahl@mandiant.com>
2015-01-21 18:04:01 -0800

    push commit info to JIRA

--- lib/services/jira.rb
+++ lib/services/jira.rb
@@ -1,13 +1,28 @@
 class Service::JIRA < Service
   string   :server_url, :api_version, :username
   password :password
-  white_list :api_version, :server_url, :username
+  boolean :post_comments
+  white_list :api_version, :server_url, :username, :post_comments
 
   def receive_push
     payload['commits'].each do |commit|
       next if commit['message'] =~ /^x /
 
+      author_display = "#{commit['author']['name']} <#{commit['author']['email']}>"
       comment_body = "#{commit['message']}\n#{commit['url']}"
+      files_changed = "\n"
+
+      # list each updated file's status in this commit
+      if config_boolean_true?('post_comments')
+        status_idx = [['A','added'],['D','removed'],['M','modified']]
+
+        # store in files_changed for post
+        status_idx.each do |abbr,file_st|
+          commit["#{file_st}"].each do |file|
+            files_changed.concat("#{abbr}\t#{file}\n")
+          end
+        end
+      end #if
 
       commit['message'].match(/\[#(.+)\]/)
       # Don't need to continue if we don't have a commit message containing JIRA markup
@@ -31,7 +46,7 @@ class Service::JIRA < Service
       end
 
       # Don't need to continue if we don't have a transition to perform
-      next unless changeset.has_key?(:transition)
+      next unless (changeset.has_key?(:transition) or config_boolean_true?('post_comments'))
 
       begin
         # :(
@@ -39,8 +54,16 @@ class Service::JIRA < Service
 
         http.basic_auth data['username'], data['password']
         http.headers['Content-Type'] = 'application/json'
-        res = http_post '%s/rest/api/%s/issue/%s/transitions' % [data['server_url'], data['api_version'], issue_id],
-          generate_json(changeset)
+        if changeset.has_key?(:transition)
+            res = http_post '%s/rest/api/%s/issue/%s/transitions' % [data['server_url'], data['api_version'], issue_id],
+              generate_json(changeset)
+        end
+
+        # add a comment containing the author, msg, and files changed
+        if config_boolean_true?('post_comments')
+              res = http_post '%s/rest/api/%s/issue/%s/comment' % [data['server_url'], data['api_version'], issue_id],
+              generate_json({ :body => "#{author_display}\n\n#{comment_body}\n\n#{files_changed}"})
+        end
       rescue URI::InvalidURIError
         raise_config_error "Invalid server_hostname: #{data['server_url']}"
       end
