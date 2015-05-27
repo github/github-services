c47e9e7316ff0eef059793238b4fc494ca606d52
Pavel Kretov <firegurafiku@gmail.com>
2015-05-27 09:35:23 +0300

    Support for refs filtering in OBS
    Introduce a new configuration parameter "refs" which contains a list
    of wildcard patterns for Git reference. Hook should only trigger build
    if the reference being pushed satisfy at least one of this patterns.
    
    This would be useful when user do not want to rebuild their packages
    on every commit pushed, but only for specific branches or tags.

--- test/obs_test.rb
+++ test/obs_test.rb
@@ -27,6 +27,16 @@ class ObsTest < Service::TestCase
     }
   end
 
+  def filter_data
+    {
+      "url" => "http://api.opensuse.org:443",
+      "token" => "github/test/token/string",
+      "project" => "home:adrianSuSE",
+      "package" => "4github",
+      "refs" => "refs/tags/version-*:refs/heads/production",
+    }
+  end
+
   def test_push_single_token
     apicall = "/trigger/runservice"
     @stubs.post apicall do |env|
@@ -60,8 +70,57 @@ class ObsTest < Service::TestCase
     assert_equal match, 2
   end
 
+  def test_filter_passed_by_tag
+    apicall = "/trigger/runservice"
+    @stubs.post apicall do |env|
+      assert_equal 'api.opensuse.org', env[:url].host
+      params = Faraday::Utils.parse_query env[:body]
+      assert_equal 'Token github/test/token/string', env[:request_headers]["Authorization"]
+      assert_equal '/trigger/runservice', env[:url].path
+      assert_equal 'package=4github&project=home%3AadrianSuSE', env[:url].query
+      [200, {}, '']
+    end
+
+    # Modify the payload to match the filter.
+    pay = payload
+    pay['ref'] = 'refs/tags/version-1.1'
+
+    svc = service :push, filter_data, pay
+    assert svc.receive
+    @stubs.verify_stubbed_calls
+  end
+
+  def test_filter_passed_by_branch
+    apicall = "/trigger/runservice"
+    @stubs.post apicall do |env|
+      assert_equal 'api.opensuse.org', env[:url].host
+      params = Faraday::Utils.parse_query env[:body]
+      assert_equal 'Token github/test/token/string', env[:request_headers]["Authorization"]
+      assert_equal '/trigger/runservice', env[:url].path
+      assert_equal 'package=4github&project=home%3AadrianSuSE', env[:url].query
+      [200, {}, '']
+    end
+
+    # Modify the payload to match the filter.
+    pay = payload
+    pay['ref'] = 'refs/heads/production'
+
+    svc = service :push, filter_data, pay
+    svc.receive
+    @stubs.verify_stubbed_calls
+  end
+
+  def test_filter_rejected
+    apicall = "/trigger/runservice"
+    @stubs.post apicall do |env|
+      flunk "Master branch should not trigger post request"
+    end
+
+    svc = service :push, filter_data, payload
+    svc.receive
+  end
+
   def service(*args)
     super Service::Obs, *args
   end
 end
-
