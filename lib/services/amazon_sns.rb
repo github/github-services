e779a49a8aa61ed21da9a2e32dea1f0d38aeb42a
Rostepher <ross.m.bayer@gmail.com>
2015-11-13 18:59:36 -0600

    Refactored new SNS message splitting functionality and added some testing to make sure it doesn't produce an error.

--- lib/services/amazon_sns.rb
+++ lib/services/amazon_sns.rb
@@ -26,60 +26,35 @@ class Service::AmazonSNS < Service
   # Maximum SNS message size is 256 kilobytes.
   MAX_MESSAGE_SIZE = 256 * 1024
 
-  # Size in bytes of each partial message.
+  # Size in bytes for each partial message.
   PARTIAL_MESSAGE_SIZE = 128 * 1024
 
   # Create a new SNS object using the AWS Ruby SDK and publish to it.
+  # If the message exceeds the maximum SNS size limit of 256K, then it will be
+  # sent in parts.
   # cfg - Configuration hash of key, secret, etc.
-  # json - THe valid JSON payload to send.
+  # json - The valid JSON payload to send.
   #
   # Returns the instantiated Amazon SNS Object
   def publish_to_sns(cfg, json)
-    begin
-      sns = Aws::SNS::Client.new({
-        :region            => cfg['sns_region'],
-        :access_key_id     => cfg['aws_key'],
-        :secret_access_key => cfg['aws_secret']
-      })
-
-      if json.bytesize <= MAX_MESSAGE_SIZE
-        sns.publish({
-          :message            => json,
-          :topic_arn          => cfg['sns_topic'],
-          :message_attributes => message_attributes
-        })
-      end
-
-      md5_digest = Digest::MD5.hexdigest(json)
-
-      payloads = split_bytes(json, PARTIAL_MESSAGE_SIZE)
-
-      messages = []
-      payloads.each_with_index do |payload, i|
-        messages << generate_json({
-          :error       => "Message exceeds the maximum SNS size limit of 256k",
-          :page_total  => payloads.length,
-          :page_number => i + 1,
-          :md5_digest  => md5_digest,
-          :message     => payload
-        })
-      end
-
-      messages.each do |message|
-        sns.publish({
-          :message            => message,
-          :topic_arn          => cfg['sns_topic'],
-          :message_attributes => message_attributes
-        })
-      end
-
-      rescue Aws::SNS::Errors::AuthorizationErrorException => e
-        raise_config_error e.message
-      rescue Aws::SNS::Errors::NotFoundException => e
-        raise_missing_error e.message
-      rescue SocketError
-        raise_missing_error
+    if json.bytesize <= MAX_MESSAGE_SIZE
+      return publish_messages_to_sns(cfg, [json])
     end
+
+    checksum = Digest::MD5.hexdigest(json)
+    payloads = split_bytes(json, PARTIAL_MESSAGE_SIZE)
+
+    messages = payloads.each_with_index.map do |payload, i|
+      generate_json({
+        :error       => "Message exceeds the maximum SNS size limit of 256K",
+        :page_total  => payloads.length,
+        :page_number => i + 1,
+        :checksum    => checksum,
+        :message     => payload
+      })
+    end
+
+    publish_messages_to_sns(cfg, messages)
   end
 
   # Build a valid AWS Configuration hash using the supplied
@@ -129,6 +104,40 @@ class Service::AmazonSNS < Service
   # Returns an array of strings.
   def split_bytes(string, n)
     string.bytes.each_slice(n).collect { |bytes| bytes.pack("C*") }
+  end
+
+  # Create a new SNS object using the AWS Ruby SDK and publish it.
+  # cfg - Configuration hash of key, secret, etc.
+  # messages - The valid JSON messages to send.
+  #
+  # Returns the instantiated Amazon SNS Object.
+  def publish_messages_to_sns(cfg, messages)
+    begin
+      sns = Aws::SNS::Client.new({
+        :region            => cfg['sns_region'],
+        :access_key_id     => cfg['aws_key'],
+        :secret_access_key => cfg['aws_secret']
+      })
+
+      messages.each_with_index do |message, i|
+        puts "message ##{i} is #{message.bytesize} bytes"
+        sns.publish({
+          :message            => message,
+          :topic_arn          => cfg['sns_topic'],
+          :message_attributes => message_attributes
+        })
+        puts "finished publishing message ##{i}"
+      end
+
+      sns
+
+    rescue Aws::SNS::Errors::AuthorizationErrorException => e
+      raise_config_error e.message
+    rescue Aws::SNS::Errors::NotFoundException => e
+      raise_missing_error e.message
+    rescue SocketError
+      raise_missing_error
+    end
   end
 
 end
