c391b10a27e0cb5537dabdbb2f579c523ccba885
Dan Wentworth <dan@atechmedia.com>
2016-02-08 11:22:18 +0000

    Add an X-Github-Event header to DeployHQ service.
    To make DeployHQ more compatible with Github webooks (now the reccomended way
    of integrating), Deploy now expects an X-Github-Event push header to be present.

--- lib/services/deploy_hq.rb
+++ lib/services/deploy_hq.rb
@@ -15,6 +15,8 @@ class Service::DeployHq < Service
 
     http.url_prefix = data['deploy_hook_url']
     http.headers['content-type'] = 'application/x-www-form-urlencoded'
+    http.headers['X-Github-Event'] = 'push'
+
     body = Faraday::Utils.build_nested_query(http.params.merge(:payload => generate_json(payload), :notify => email_pusher))
 
     http_post data['deploy_hook_url'], body
