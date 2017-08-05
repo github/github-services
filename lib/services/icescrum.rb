5bf6dffd9c683072462b2b15539c93da8a39c355
Vincent Barrier <vbarrier@kagilum.com>
2017-08-05 10:26:12 +0200

    Update icescrum.rb
    Add access token support

--- lib/services/icescrum.rb
+++ lib/services/icescrum.rb
@@ -1,26 +1,35 @@
 class Service::IceScrum < Service
-  string   :base_url, :project_key, :username
+  string   :base_url, :access_token, :project_key, :username
   password :password
   def receive_push
-	raise_config_error "Invalid username" if data['username'].to_s.empty?
-	raise_config_error "Invalid password" if data['password'].to_s.empty?
-	raise_config_error "Invalid project key" if data['project_key'].to_s.empty?
-
-	username = data['username'].to_s.gsub(/\s+/, "")
-	project_key = data['project_key'].to_s.upcase.gsub(/\s+/, "")
-	password = data['password'].to_s.gsub(/\s+/, "")
-
+	# Support old authentification for R6
+	if data['access_token'].to_s.empty?
+		raise_config_error "Invalid username" if data['username'].to_s.empty?
+		raise_config_error "Invalid password" if data['password'].to_s.empty?
+	end
+	
+	# project key always mandatory
+	raise_config_error "Invalid project key" if data['project_key'].to_s.empty?	
+	project_key = data['project_key'].to_s.upcase.gsub(/\s+/, "")	
+	  
+	# setup base url
 	if data['base_url'].present?
-    	    url = "#{data['base_url']}/ws/p/#{project_key}/commit"
-       else
-	    url = "https://www.kagilum.com/a/ws/p/#{project_key}/commit"
-       end
+    		url = "#{data['base_url']}/ws/p/#{project_key}/commit"
+        else
+		url = "https://cloud.icescrum.com/ws/p/#{project_key}/commit"
+        end
 
-  	http.ssl[:verify] = false
-	http.basic_auth username, password
-
+	# do old basic authentication
+	if data['access_token'].to_s.empty?
+		username = data['username'].to_s.gsub(/\s+/, "")
+		password = data['password'].to_s.gsub(/\s+/, "")
+  		http.ssl[:verify] = false
+		http.basic_auth username, password
+	else
+		http.headers['Content-Type'] = 'application/json'
+		http.headers['x-icescrum-token'] = data['access_token'].to_s.gsub(/\s+/, "")
+	end
 	http_post url, { :payload => generate_json(payload) }
   end
 
 end
-
